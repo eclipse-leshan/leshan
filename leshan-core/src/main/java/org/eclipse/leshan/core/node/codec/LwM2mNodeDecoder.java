86909863d5900f77954736bf8680df5567329a65
Julien Vermillard <jvermillard@sierrawireless.com>
2015-04-28 09:29:56 +0200

    fixed array out of bound exception introduced by e73facd

--- leshan-core/src/main/java/org/eclipse/leshan/core/node/codec/LwM2mNodeDecoder.java
+++ leshan-core/src/main/java/org/eclipse/leshan/core/node/codec/LwM2mNodeDecoder.java
@@ -180,8 +180,8 @@ public class LwM2mNodeDecoder {
             ObjectModel objectModel = model.getObjectModel(path.getObjectId());
             boolean multiple = objectModel == null ? true : objectModel.multiple;
 
-            if (!multiple && tlvs.length > 0 && tlvs[0].getType() == TlvType.MULTIPLE_RESOURCE
-                    || tlvs[0].getType() == TlvType.RESOURCE_VALUE) {
+            if (!multiple && tlvs.length > 0 && //
+                    (tlvs[0].getType() == TlvType.MULTIPLE_RESOURCE || tlvs[0].getType() == TlvType.RESOURCE_VALUE)) {
                 LwM2mResource[] resources = new LwM2mResource[tlvs.length];
                 for (int i = 0; i < tlvs.length; i++) {
                     resources[i] = parseResourceTlv(tlvs[i], path.getObjectId(), 0, model);
