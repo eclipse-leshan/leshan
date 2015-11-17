52acec58ab099027da7d9d509894c7537306d104
Simon Bernard <sbernard@sierrawireless.com>
2015-11-17 14:25:34 +0100

    Add javadoc to write request

--- leshan-core/src/main/java/org/eclipse/leshan/core/request/WriteRequest.java
+++ leshan-core/src/main/java/org/eclipse/leshan/core/request/WriteRequest.java
@@ -36,8 +36,20 @@ import org.eclipse.leshan.util.Validate;
  */
 public class WriteRequest extends AbstractDownlinkRequest<WriteResponse> {
 
+    /**
+     * Define the behavior of a write request.
+     */
     public enum Mode {
-        REPLACE, UPDATE
+        /**
+         * Replaces the Object Instance or the Resource(s) with the new value provided in the “Write” operation. (see
+         * section 5.3.3 of the LW M2M spec).
+         */
+        REPLACE,
+        /**
+         * Adds or updates Resources or Resource Instances provided in the new value and leaves other existing Resources
+         * or Resource Instances unchanged. (see section 5.3.3 of the LW M2M spec).
+         */
+        UPDATE
     }
 
     private final LwM2mNode node;
@@ -46,44 +58,108 @@ public class WriteRequest extends AbstractDownlinkRequest<WriteResponse> {
 
     // ***************** write object ****************** //
 
+    /**
+     * Request to write an <b>Object</b>.
+     * 
+     * @param mode the mode of the request : replace or update.
+     * @param contentFormat Format of the payload (TLV or JSON).
+     * @param objectId the id of the object to write.
+     * @param instances the list of instance object to write.
+     */
     public WriteRequest(final Mode mode, final ContentFormat contentFormat, final int objectId,
             final Collection<LwM2mObjectInstance> instances) {
         this(mode, contentFormat, new LwM2mPath(objectId), new LwM2mObject(objectId, instances));
     }
 
+    /**
+     * Request to write an <b>Object</b> using the TLV content format.
+     * 
+     * @param mode the mode of the request : replace or update.
+     * @param objectId the id of the object to write.
+     * @param instances the list of instance object to write.
+     */
     public WriteRequest(final Mode mode, final int objectId, final Collection<LwM2mObjectInstance> instances) {
         this(mode, ContentFormat.TLV, new LwM2mPath(objectId), new LwM2mObject(objectId, instances));
     }
 
+    /**
+     * Request to write an <b>Object</b> using the TLV content format.
+     * 
+     * @param mode the mode of the request : replace or update.
+     * @param contentFormat Format of the payload (TLV or JSON).
+     * @param objectId the id of the object to write.
+     * @param instances the list of instance object to write.
+     */
     public WriteRequest(final Mode mode, final ContentFormat contentFormat, final int objectId,
             final LwM2mObjectInstance... instances) {
         this(mode, contentFormat, new LwM2mPath(objectId), new LwM2mObject(objectId, instances));
     }
 
+    /**
+     * Request to write an <b>Object</b> using the TLV content format.
+     * 
+     * @param mode the mode of the request : replace or update.
+     * @param objectId the id of the object to write.
+     * @param instances the list of instance object to write.
+     */
     public WriteRequest(final Mode mode, final int objectId, final LwM2mObjectInstance... instances) {
         this(mode, ContentFormat.TLV, new LwM2mPath(objectId), new LwM2mObject(objectId, instances));
     }
 
     // ***************** write instance ****************** //
 
+    /**
+     * Request to write an <b>Object Instance</b>.
+     * 
+     * @param mode the mode of the request : replace or update.
+     * @param contentFormat Format of the payload (TLV or JSON).
+     * @param objectId the id of the object to write.
+     * @param objectInstanceId the id of the object instance to write.
+     * @param resources the list of resources to write.
+     */
     public WriteRequest(final Mode mode, final ContentFormat contentFormat, final int objectId,
             final int objectInstanceId, final Collection<LwM2mResource> resources) {
         this(mode, contentFormat, new LwM2mPath(objectId, objectInstanceId), new LwM2mObjectInstance(objectId,
                 resources));
     }
 
+    /**
+     * Request to write an <b>Object Instance</b> using the TLV content format.
+     * 
+     * @param mode the mode of the request : replace or update.
+     * @param objectId the id of the object to write.
+     * @param objectInstanceId the id of the object instance to write.
+     * @param resources the list of resources to write.
+     */
     public WriteRequest(final Mode mode, final int objectId, final int objectInstanceId,
             final Collection<LwM2mResource> resources) {
         this(mode, ContentFormat.TLV, new LwM2mPath(objectId, objectInstanceId), new LwM2mObjectInstance(objectId,
                 resources));
     }
 
+    /**
+     * Request to write an <b>Object Instance</b>.
+     * 
+     * @param mode the mode of the request : replace or update.
+     * @param contentFormat Format of the payload (TLV or JSON).
+     * @param objectId the id of the object to write.
+     * @param objectInstanceId the id of the object instance to write.
+     * @param resources the list of resources to write.
+     */
     public WriteRequest(final Mode mode, final ContentFormat contentFormat, final int objectId,
             final int objectInstanceId, final LwM2mResource... resources) {
         this(mode, contentFormat, new LwM2mPath(objectId, objectInstanceId), new LwM2mObjectInstance(objectId,
                 resources));
     }
 
+    /**
+     * Request to write an <b>Object Instance</b> using the TLV content format.
+     * 
+     * @param mode the mode of the request : replace or update.
+     * @param objectId the id of the object to write.
+     * @param objectInstanceId the id of the object instance to write.
+     * @param resources the list of resources to write.
+     */
     public WriteRequest(final Mode mode, final int objectId, final int objectInstanceId,
             final LwM2mResource... resources) {
         this(mode, ContentFormat.TLV, new LwM2mPath(objectId, objectInstanceId), new LwM2mObjectInstance(objectId,
@@ -91,36 +167,54 @@ public class WriteRequest extends AbstractDownlinkRequest<WriteResponse> {
     }
 
     // ***************** write single value resource ****************** //
+    /**
+     * Request to write a <b>String Single-Instance Resource</b> using the TEXT content format.
+     */
     public WriteRequest(final Mode mode, final int objectId, final int objectInstanceId, final int resourceId,
             String value) {
         this(mode, ContentFormat.TEXT, new LwM2mPath(objectId, objectInstanceId, resourceId), LwM2mSingleResource
                 .newStringResource(resourceId, value));
     }
 
+    /**
+     * Request to write a <b>Boolean Single-Instance Resource</b> using the TEXT content format.
+     */
     public WriteRequest(final Mode mode, final int objectId, final int objectInstanceId, final int resourceId,
             boolean value) {
         this(mode, ContentFormat.TEXT, new LwM2mPath(objectId, objectInstanceId, resourceId), LwM2mSingleResource
                 .newBooleanResource(resourceId, value));
     }
 
+    /**
+     * Request to write a <b>Integer Single-Instance Resource</b> using the TEXT content format.
+     */
     public WriteRequest(final Mode mode, final int objectId, final int objectInstanceId, final int resourceId,
             long value) {
         this(mode, ContentFormat.TEXT, new LwM2mPath(objectId, objectInstanceId, resourceId), LwM2mSingleResource
                 .newIntegerResource(resourceId, value));
     }
 
+    /**
+     * Request to write a <b> Float Single-Instance Resource</b> using the TEXT content format.
+     */
     public WriteRequest(final Mode mode, final int objectId, final int objectInstanceId, final int resourceId,
             double value) {
         this(mode, ContentFormat.TEXT, new LwM2mPath(objectId, objectInstanceId, resourceId), LwM2mSingleResource
                 .newFloatResource(resourceId, value));
     }
 
+    /**
+     * Request to write a <b> Date Single-Instance Resource</b> using the TEXT content format.
+     */
     public WriteRequest(final Mode mode, final int objectId, final int objectInstanceId, final int resourceId,
             Date value) {
         this(mode, ContentFormat.TEXT, new LwM2mPath(objectId, objectInstanceId, resourceId), LwM2mSingleResource
                 .newDateResource(resourceId, value));
     }
 
+    /**
+     * Request to write a <b> Binary Single-Instance Resource</b> using the OPAQUE content format.
+     */
     public WriteRequest(final Mode mode, final int objectId, final int objectInstanceId, final int resourceId,
             byte[] value) {
         this(mode, ContentFormat.OPAQUE, new LwM2mPath(objectId, objectInstanceId, resourceId), LwM2mSingleResource
@@ -128,12 +222,33 @@ public class WriteRequest extends AbstractDownlinkRequest<WriteResponse> {
     }
 
     // ***************** write multi instance resource ****************** //
+    /**
+     * Request to write a <b>Multi-Instance Resource</b>.
+     * 
+     * @param mode the mode of the request : replace or update.
+     * @param contentFormat Format of the payload (TLV or JSON).
+     * @param objectId the id of the object to write.
+     * @param objectInstanceId the id of the object instance to write.
+     * @param resourceId the id of the resource to write.
+     * @param values the list of resource instance (id->value) to write.
+     * @param type the data type of the resource.
+     */
     public WriteRequest(final Mode mode, final ContentFormat contentFormat, final int objectId,
             final int objectInstanceId, final int resourceId, final Map<Integer, ?> values, Type type) {
         this(mode, contentFormat, new LwM2mPath(objectId, objectInstanceId, resourceId), LwM2mMultipleResource
                 .newResource(resourceId, values, type));
     }
 
+    /**
+     * Request to write a <b>Multi-Instance Resource</b> using the TLV content format.
+     * 
+     * @param mode the mode of the request : replace or update.
+     * @param objectId the id of the object to write.
+     * @param objectInstanceId the id of the object instance to write.
+     * @param resourceId the id of the resource to write.
+     * @param values the list of resource instance (id->value) to write.
+     * @param type the data type of the resource.
+     */
     public WriteRequest(final Mode mode, final int objectId, final int objectInstanceId, final int resourceId,
             final Map<Integer, ?> values, Type type) {
         this(mode, ContentFormat.TLV, new LwM2mPath(objectId, objectInstanceId, resourceId), LwM2mMultipleResource
@@ -141,6 +256,14 @@ public class WriteRequest extends AbstractDownlinkRequest<WriteResponse> {
     }
 
     // ***************** generic constructor ****************** //
+    /**
+     * A generic constructor to write request.
+     * 
+     * @param mode the mode of the request : replace or update.
+     * @param contentFormat Format of the payload (TLV,JSON,TEXT,OPAQUE ..).
+     * @param path the path of the resource to write.
+     * @param node the {@link LwM2mNode} to write.
+     */
     public WriteRequest(final Mode mode, final ContentFormat contentFormat, final String path, final LwM2mNode node) {
         this(mode, contentFormat, new LwM2mPath(path), node);
     }
