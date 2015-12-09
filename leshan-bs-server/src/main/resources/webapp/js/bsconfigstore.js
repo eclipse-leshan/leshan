706000cfa4fc449ff02a22d1c4b1610a65db0582
Simon Bernard <sbernard@sierrawireless.com>
2015-12-09 17:00:18 +0100

    add very simple web ui to bootstrap server

---
+++ leshan-bs-server/src/main/resources/webapp/js/bsconfigstore.js
@@ -0,0 +1,102 @@
+// convert config from server format to UI format :
+// we regroup security and server data
+var convertConfig = function(config){
+    var newConfig = {dm:[],bs:[]};
+    for (var i in config.security){
+        var security = config.security[i];
+        if (security.bootstrapServer){
+            newConfig.bs.push({security:security});
+        }else{
+            newConfig.dm = [];
+            // search for DM information;
+            for (var j in config.servers){
+                var server = config.servers[j];
+                if (server.shortId === security.serverId){
+                    newConfig.dm.push(server);
+                    server.security = security;
+                }
+            }
+            if (!newConfig.dm){
+                newConfig.dm.push({security:security});
+            }
+        }
+    }
+    return newConfig;
+};
+var convertConfigs = function(configs){
+    var newConfigs = {};
+    for (var endpoint in configs){
+        newConfigs[endpoint] = convertConfig(configs[endpoint]);
+    }
+    return newConfigs;
+};
+
+//convert config from UI to UI server:
+var toConfig = function(config){
+    var newConfig = {servers:{}, security:{}};
+    for (var i = 0; i < config.bs.length; i++) {
+        var bs = config.bs[i];
+        newConfig.security[i] = bs.security;
+    }
+    for (var j = 0; j < config.dm.length; j++) {
+        var dm = config.dm[j];
+        newConfig.security[i+j] = dm.security;
+        delete dm.security;
+        newConfig.servers[j] = dm;
+    }
+    return newConfig;
+};
+
+// A bootstrap config store
+function BsConfigStore() {
+    riot.observable(this); // Riot provides our event emitter.
+
+    var self = this;
+
+    self.bsconfigs = {};
+
+    self.init = function (){
+        $.get('api/bootstrap', function(data) {
+            self.bsconfigs = convertConfigs(data);
+            self.trigger('changed', self.bsconfigs);
+        }).fail(function(data, status, headers, config){
+            var err = "Unable to get the bootstrap info list.";
+            console.error(err, status, data);
+            alert(err + " see console log for mor details");
+        });
+    };
+
+    self.add = function(endpoint,config) {
+        var data = toConfig(config);
+        console.log(data);
+        $.ajax({
+            type: "POST",
+            url: 'api/bootstrap/'+endpoint,
+            data: JSON.stringify(data),
+            contentType:"application/json; charset=utf-8",
+        }).done(function () {
+            self.bsconfigs[endpoint] = convertConfig(data);
+            self.trigger('changed', self.bsconfigs);
+        })
+        .fail(function (response, status) {
+          var err = "Unable to post the bootstrap config.";
+          console.error(err,endpoint, status, response);
+          alert(err + "see console log for mor details");
+        });
+    };
+
+    self.remove = function(endpoint) {
+        $.ajax({
+            type: "DELETE",
+            url: 'api/bootstrap/'+endpoint,
+        }).done(function () {
+            delete self.bsconfigs[endpoint];
+            self.trigger('changed', self.bsconfigs);
+        })
+        .fail(function (response, status) {
+          var err = "Unable to delete the bootstrap config.";
+          console.error(err,endpoint, status, response);
+          alert(err + "see console log for mor details");
+        });
+    };
+}
