9385c9520201675ee1f7a605d81b2c441fd1df3b
Pierre-Henri Trivier <phtrivier@sierrawireless.com>
2016-01-20 18:10:30 -0800

    Last Update in client list

--- leshan-standalone/src/main/java/org/eclipse/leshan/standalone/servlet/json/ClientSerializer.java
+++ leshan-standalone/src/main/java/org/eclipse/leshan/standalone/servlet/json/ClientSerializer.java
@@ -39,6 +39,7 @@ public class ClientSerializer implements JsonSerializer<Client> {
         element.addProperty("endpoint", src.getEndpoint());
         element.addProperty("registrationId", src.getRegistrationId());
         element.add("registrationDate", context.serialize(src.getRegistrationDate()));
+        element.add("lastUpdate", context.serialize(src.getLastUpdate()));
         element.addProperty("address", src.getAddress().toString() + ":" + src.getPort());
         element.addProperty("smsNumber", src.getSmsNumber());
         element.addProperty("lwM2MmVersion", src.getLwM2mVersion());
