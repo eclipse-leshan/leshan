831c03870c0ca41872584f6d8ae1f310845a87a6
Simon Bernard <sbernard@sierrawireless.com>
2016-03-08 17:01:09 +0100

    Reimplement observe based on experimental Californium observe impl.

--- leshan-server-cf/src/test/java/org/eclipse/leshan/server/californium/impl/CaliforniumObservationTest.java
+++ leshan-server-cf/src/test/java/org/eclipse/leshan/server/californium/impl/CaliforniumObservationTest.java
@@ -15,106 +15,51 @@
  *******************************************************************************/
 package org.eclipse.leshan.server.californium.impl;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-
+import java.util.Random;
 import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
 
-import org.eclipse.californium.core.coap.CoAP.ResponseCode;
 import org.eclipse.californium.core.coap.Request;
-import org.eclipse.californium.core.coap.Response;
-import org.eclipse.leshan.core.model.LwM2mModel;
-import org.eclipse.leshan.core.model.ObjectLoader;
-import org.eclipse.leshan.core.node.LwM2mNode;
+import org.eclipse.californium.core.observe.InMemoryObserveRequestStore;
 import org.eclipse.leshan.core.node.LwM2mPath;
-import org.eclipse.leshan.core.node.LwM2mResource;
 import org.eclipse.leshan.core.observation.Observation;
-import org.eclipse.leshan.core.observation.ObservationListener;
-import org.eclipse.leshan.server.impl.ObservationRegistryImpl;
+import org.eclipse.leshan.server.californium.CaliforniumObservationRegistry;
+import org.eclipse.leshan.server.impl.ClientRegistryImpl;
+import org.eclipse.leshan.server.model.StandardModelProvider;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CaliforniumObservationTest {
 
-    final String reportedValue = "15";
     Request coapRequest;
     LwM2mPath target;
+    CaliforniumObservationRegistry registry;
 
     private CaliforniumTestSupport support = new CaliforniumTestSupport();
-
-    private static LwM2mModel model;
-
-    @BeforeClass
-    public static void loadModel() {
-        model = new LwM2mModel(ObjectLoader.loadDefault());
-    }
 
     @Before
     public void setUp() throws Exception {
         support.givenASimpleClient();
-        target = new LwM2mPath(3, 0, 15);
+        registry = new CaliforniumObservationRegistryImpl(new InMemoryObserveRequestStore(), new ClientRegistryImpl(),
+                new StandardModelProvider());
     }
 
     @Test
-    public void coapNotification_is_forwarded_to_observationListener() {
+    public void cancel_by_client() {
+        // create some observations and add it to registry
+        givenAnObservation(support.client.getRegistrationId(), new LwM2mPath(3, 0, 13));
+        givenAnObservation(support.client.getRegistrationId(), new LwM2mPath(3, 0, 12));
 
-        ObservationListener listener = new ObservationListener() {
-            @Override
-            public void newValue(Observation observation, LwM2mNode value) {
-                assertEquals(CaliforniumObservationTest.this.reportedValue, ((LwM2mResource) value).getValue());
-                assertEquals(3, observation.getPath().getObjectId());
-                assertEquals((Integer) 0, observation.getPath().getObjectInstanceId());
-                assertEquals((Integer) 15, observation.getPath().getResourceId());
-                assertEquals(CaliforniumObservationTest.this.support.client.getRegistrationId(),
-                        observation.getRegistrationId());
-            }
-
-            @Override
-            public void cancelled(Observation observation) {
-            }
-        };
-        givenAnObserveRequest(target);
-        CaliforniumObservation observation = new CaliforniumObservation(coapRequest,
-                support.client.getRegistrationId(), target, model);
-        observation.addListener(listener);
-        Response coapResponse = new Response(ResponseCode.CONTENT);
-        coapResponse.setPayload(reportedValue);
-        observation.onResponse(coapResponse);
-    }
-
-    @Test
-    public void cancel_Observation_cancel_coapRequest() {
-
-        givenAnObserveRequest(target);
-        assertFalse(coapRequest.isCanceled());
-        CaliforniumObservation observation = new CaliforniumObservation(coapRequest,
-                support.client.getRegistrationId(), target, model);
-        observation.cancel();
-        Assert.assertTrue(coapRequest.isCanceled());
-    }
-
-    @Test
-    public void cancelled_Observation_are_removed_from_registry() {
-        // create an observation
-        givenAnObserveRequest(target);
-        CaliforniumObservation observation = new CaliforniumObservation(coapRequest,
-                support.client.getRegistrationId(), target, model);
-        coapRequest.addMessageObserver(observation);
-
-        // add it to registry
-        ObservationRegistryImpl registry = new ObservationRegistryImpl();
-        registry.addObservation(observation);
+        givenAnObservation("anotherClient", new LwM2mPath(3, 0, 12));
 
         // check its presence
         Set<Observation> observations = registry.getObservations(support.client);
-        Assert.assertFalse(observations.isEmpty());
-        Assert.assertFalse(coapRequest.isCanceled());
+        Assert.assertEquals(2, observations.size());
 
         // cancel it
-        observation.cancel();
-        Assert.assertTrue(coapRequest.isCanceled());
+        int nbCancelled = registry.cancelObservations(support.client);
+        Assert.assertEquals(2, nbCancelled);
 
         // check its absence
         observations = registry.getObservations(support.client);
@@ -122,37 +67,70 @@ public class CaliforniumObservationTest {
     }
 
     @Test
-    public void cancelled_from_registry_observation_are_removed_from_registry() {
-        // create an observation
-        givenAnObserveRequest(target);
-        CaliforniumObservation observation = new CaliforniumObservation(coapRequest,
-                support.client.getRegistrationId(), target, model);
-        coapRequest.addMessageObserver(observation);
+    public void cancel_by_path() {
+        // create some observations and add it to registry
+        givenAnObservation(support.client.getRegistrationId(), new LwM2mPath(3, 0, 13));
+        givenAnObservation(support.client.getRegistrationId(), new LwM2mPath(3, 0, 12));
+        givenAnObservation(support.client.getRegistrationId(), new LwM2mPath(3, 0, 12));
 
-        // add it to registry
-        ObservationRegistryImpl registry = new ObservationRegistryImpl();
-        registry.addObservation(observation);
+        givenAnObservation("anotherClient", new LwM2mPath(3, 0, 12));
 
         // check its presence
         Set<Observation> observations = registry.getObservations(support.client);
-        Assert.assertFalse(observations.isEmpty());
-        Assert.assertFalse(coapRequest.isCanceled());
+        Assert.assertEquals(3, observations.size());
 
         // cancel it
-        registry.cancelObservations(support.client);
-        Assert.assertTrue(coapRequest.isCanceled());
+        int nbCancelled = registry.cancelObservations(support.client, "/3/0/12");
+        Assert.assertEquals(2, nbCancelled);
 
         // check its absence
         observations = registry.getObservations(support.client);
-        Assert.assertTrue(observations.isEmpty());
+        Assert.assertEquals(1, observations.size());
     }
 
-    private void givenAnObserveRequest(LwM2mPath target) {
+    @Test
+    public void cancel_by_observation() {
+        // create some observations and add it to registry
+        givenAnObservation(support.client.getRegistrationId(), new LwM2mPath(3, 0, 13));
+        givenAnObservation(support.client.getRegistrationId(), new LwM2mPath(3, 0, 12));
+        givenAnObservation("anotherClient", new LwM2mPath(3, 0, 12));
+
+        Observation observationToCancel = givenAnObservation(support.client.getRegistrationId(),
+                new LwM2mPath(3, 0, 12));
+
+        // check its presence
+        Set<Observation> observations = registry.getObservations(support.client);
+        Assert.assertEquals(3, observations.size());
+
+        // cancel it
+        registry.cancelObservation(observationToCancel);
+
+        // check its absence
+        observations = registry.getObservations(support.client);
+        Assert.assertEquals(2, observations.size());
+    }
+
+    private Observation givenAnObservation(String registrationId, LwM2mPath target) {
+
         coapRequest = Request.newGet();
+        coapRequest.setToken(createToken());
         coapRequest.getOptions().addUriPath(String.valueOf(target.getObjectId()));
         coapRequest.getOptions().addUriPath(String.valueOf(target.getObjectInstanceId()));
         coapRequest.getOptions().addUriPath(String.valueOf(target.getResourceId()));
         coapRequest.setDestination(support.client.getAddress());
         coapRequest.setDestinationPort(support.client.getPort());
+        registry.getObserveRequestStore().add(coapRequest);
+        Observation observation = new Observation(coapRequest.getToken(), registrationId, target);
+        registry.addObservation(observation);
+        return observation;
+    }
+
+    private byte[] createToken() {
+        Random random = ThreadLocalRandom.current();
+        byte[] token;
+        token = new byte[random.nextInt(8) + 1];
+        // random value
+        random.nextBytes(token);
+        return token;
     }
 }
