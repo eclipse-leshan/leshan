020e3e0c8c95d2ea77dc0439a3267fd5972a31b7
Simon Bernard <sbernard@sierrawireless.com>
2016-07-26 15:08:54 +0200

    add toString() equals() method to ContentFormat.

--- leshan-core/src/main/java/org/eclipse/leshan/core/request/ContentFormat.java
+++ leshan-core/src/main/java/org/eclipse/leshan/core/request/ContentFormat.java
@@ -100,4 +100,31 @@ public class ContentFormat {
         }
         return null;
     }
+
+    @Override
+    public String toString() {
+        return String.format("ContentFormat [name=%s, code=%s]", name, code);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + code;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ContentFormat other = (ContentFormat) obj;
+        if (code != other.code)
+            return false;
+        return true;
+    }
 }
