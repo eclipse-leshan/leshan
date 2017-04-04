15e1111345c69e6c77e3aa3f74bd8c2167851f0d
Simon Bernard <sbernard@sierrawireless.com>
2017-04-04 16:32:48 +0200

    Add javadoc to LeshanServerBuilder.

--- leshan-server-cf/src/main/java/org/eclipse/leshan/server/californium/LeshanServerBuilder.java
+++ leshan-server-cf/src/main/java/org/eclipse/leshan/server/californium/LeshanServerBuilder.java
@@ -24,17 +24,23 @@ import java.security.cert.X509Certificate;
 
 import org.eclipse.californium.core.network.config.NetworkConfig;
 import org.eclipse.leshan.LwM2m;
+import org.eclipse.leshan.core.node.LwM2mNode;
 import org.eclipse.leshan.core.node.codec.DefaultLwM2mNodeDecoder;
 import org.eclipse.leshan.core.node.codec.DefaultLwM2mNodeEncoder;
 import org.eclipse.leshan.core.node.codec.LwM2mNodeDecoder;
 import org.eclipse.leshan.core.node.codec.LwM2mNodeEncoder;
+import org.eclipse.leshan.core.observation.Observation;
 import org.eclipse.leshan.server.LwM2mServer;
 import org.eclipse.leshan.server.californium.impl.InMemoryRegistrationStore;
 import org.eclipse.leshan.server.californium.impl.LeshanServer;
+import org.eclipse.leshan.server.impl.InMemorySecurityStore;
 import org.eclipse.leshan.server.model.LwM2mModelProvider;
 import org.eclipse.leshan.server.model.StandardModelProvider;
+import org.eclipse.leshan.server.registration.Registration;
+import org.eclipse.leshan.server.registration.RegistrationStore;
 import org.eclipse.leshan.server.security.Authorizer;
 import org.eclipse.leshan.server.security.DefaultAuthorizer;
+import org.eclipse.leshan.server.security.SecurityInfo;
 import org.eclipse.leshan.server.security.SecurityStore;
 
 /**
@@ -62,6 +68,17 @@ public class LeshanServerBuilder {
 
     private NetworkConfig networkConfig;
 
+    /**
+     * <p>
+     * Set the address/port for unsecured CoAP Server.
+     * </p>
+     * 
+     * By default a wildcard address and the default CoAP port(5683) is used
+     * 
+     * @param hostname The address to bind. If null wildcard address is used.
+     * @param port A valid port value is between 0 and 65535. A port number of zero will let the system pick up an
+     *        ephemeral port in a bind operation.
+     */
     public LeshanServerBuilder setLocalAddress(String hostname, int port) {
         if (hostname == null) {
             this.localAddress = new InetSocketAddress(port);
@@ -71,11 +88,29 @@ public class LeshanServerBuilder {
         return this;
     }
 
+    /**
+     * <p>
+     * Set the address for unsecured CoAP Server.
+     * </p>
+     * 
+     * By default a wildcard address and the default CoAP port(5683) is used.
+     */
     public LeshanServerBuilder setLocalAddress(InetSocketAddress localAddress) {
         this.localAddress = localAddress;
         return this;
     }
 
+    /**
+     * <p>
+     * Set the address/port for secured CoAP Server (Using DTLS).
+     * <p>
+     * 
+     * By default a wildcard address and the default CoAPs port(5684) is used.
+     * 
+     * @param hostname The address to bind. If null wildcard address is used.
+     * @param port A valid port value is between 0 and 65535. A port number of zero will let the system pick up an
+     *        ephemeral port in a bind operation.
+     */
     public LeshanServerBuilder setLocalSecureAddress(String hostname, int port) {
         if (hostname == null) {
             this.localSecureAddress = new InetSocketAddress(port);
@@ -85,61 +120,133 @@ public class LeshanServerBuilder {
         return this;
     }
 
+    /**
+     * <p>
+     * Set the address for secured CoAP Server (Using DTLS).
+     * </p>
+     * 
+     * By default a wildcard address and the default CoAP port(5684) is used.
+     */
     public LeshanServerBuilder setLocalSecureAddress(InetSocketAddress localSecureAddress) {
         this.localSecureAddress = localSecureAddress;
         return this;
     }
 
+    /**
+     * <p>
+     * Set your {@link RegistrationStore} implementation which stores {@link Registration} and {@link Observation}.
+     * </p>
+     * By default the {@link InMemoryRegistrationStore} implementation is used.
+     * 
+     */
     public LeshanServerBuilder setRegistrationStore(CaliforniumRegistrationStore registrationStore) {
         this.registrationStore = registrationStore;
         return this;
     }
 
+    /**
+     * <p>
+     * Set your {@link SecurityStore} implementation which stores {@link SecurityInfo}.
+     * </p>
+     * By default no security store is set and the secure CoAP Server is not launched. An {@link InMemorySecurityStore}
+     * is provided to start using secure connection.
+     * 
+     */
     public LeshanServerBuilder setSecurityStore(SecurityStore securityStore) {
         this.securityStore = securityStore;
         return this;
     }
 
+    /**
+     * <p>
+     * Set your {@link Authorizer} implementation to define if a device if authorize to register to this server.
+     * </p>
+     * By default the {@link DefaultAuthorizer} implementation is used, if a security store is set.
+     * 
+     */
     public LeshanServerBuilder setAuthorizer(Authorizer authorizer) {
         this.authorizer = authorizer;
         return this;
     }
 
+    /**
+     * <p>
+     * Set your {@link LwM2mModelProvider} implementation.
+     * </p>
+     * By default the {@link StandardModelProvider} implementation is used which support all core objects for all
+     * devices.
+     * 
+     */
     public LeshanServerBuilder setObjectModelProvider(LwM2mModelProvider objectModelProvider) {
         this.modelProvider = objectModelProvider;
         return this;
     }
 
+    /**
+     * <p>
+     * Set the {@link PublicKey} of the server which will be used for RawPublicKey DTLS authentication.
+     * </p>
+     * This should be used for RPK support only. If you support RPK and X509,
+     * {@link LeshanServerBuilder#setCertificateChain(X509Certificate[])} should be used.
+     */
     public LeshanServerBuilder setPublicKey(PublicKey publicKey) {
         this.publicKey = publicKey;
         return this;
     }
 
+    /**
+     * Set the {@link PrivateKey} of the server which will be used for RawPublicKey(RPK) and X509 DTLS authentication.
+     */
     public LeshanServerBuilder setPrivateKey(PrivateKey privateKey) {
         this.privateKey = privateKey;
         return this;
     }
 
+    /**
+     * <p>
+     * Set the CertificateChain of the server which will be used for X509 DTLS authentication.
+     * </p>
+     * For RPK the public key will be extract from the first X509 certificate of the certificate chain. If you only need
+     * RPK support, use {@link LeshanServerBuilder#setPublicKey(PublicKey)} instead.
+     */
     public LeshanServerBuilder setCertificateChain(X509Certificate[] certificateChain) {
         this.certificateChain = certificateChain;
         return this;
     }
 
+    /**
+     * The list of trusted certificates used to authenticate devices.
+     */
     public LeshanServerBuilder setTrustedCertificates(Certificate[] trustedCertificates) {
         this.trustedCertificates = trustedCertificates;
         return this;
     }
 
+    /**
+     * <p>
+     * Set the {@link LwM2mNodeEncoder} which will encode {@link LwM2mNode} with supported content format.
+     * </p>
+     * By default the {@link DefaultLwM2mNodeEncoder} is used. It supports Text, Opaque, TLV and JSON format.
+     */
     public LeshanServerBuilder setEncoder(LwM2mNodeEncoder encoder) {
         this.encoder = encoder;
         return this;
     }
 
+    /**
+     * <p>
+     * Set the {@link LwM2mNodeDecoder} which will decode data in supported content format to create {@link LwM2mNode}.
+     * </p>
+     * By default the {@link DefaultLwM2mNodeDecoder} is used. It supports Text, Opaque, TLV and JSON format.
+     */
     public LeshanServerBuilder setDecoder(LwM2mNodeDecoder decoder) {
         this.decoder = decoder;
         return this;
     }
 
+    /**
+     * Set the Californium/CoAP {@link NetworkConfig}.
+     */
     public LeshanServerBuilder setNetworkConfig(NetworkConfig config) {
         this.networkConfig = config;
         return this;
