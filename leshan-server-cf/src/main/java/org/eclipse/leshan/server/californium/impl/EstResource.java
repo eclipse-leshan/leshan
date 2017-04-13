313a7616d6797f99c675998731e43b79e9022198
Manuel Sangoi <msangoi@sierrawireless.com>
2017-04-13 17:34:49 +0200

    bouncycastle 1.52 implementation

--- leshan-server-cf/src/main/java/org/eclipse/leshan/server/californium/impl/EstResource.java
+++ leshan-server-cf/src/main/java/org/eclipse/leshan/server/californium/impl/EstResource.java
@@ -15,13 +15,15 @@
  *******************************************************************************/
 package org.eclipse.leshan.server.californium.impl;
 
-import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
-import java.security.SignatureException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.bouncycastle.asn1.ASN1Encodable;
+import org.bouncycastle.asn1.ASN1Sequence;
+import org.bouncycastle.asn1.pkcs.CertificationRequest;
+import org.bouncycastle.asn1.x509.Attribute;
+import org.bouncycastle.asn1.x509.X509ObjectIdentifiers;
 import org.eclipse.californium.core.CoapResource;
 import org.eclipse.californium.core.coap.CoAP.ResponseCode;
 import org.eclipse.californium.core.coap.Request;
@@ -35,14 +37,10 @@ import org.eclipse.leshan.server.security.SecurityInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import sun.security.pkcs10.PKCS10;
-import sun.security.pkcs10.PKCS10Attribute;
-
 /**
  * Implements the needed CoAP EST resources, based on https://tools.ietf.org/html/draft-vanderstok-ace-coap-est-01 URLs:
  * /est/crts (CA certs) /est/sen (simple enroll) /est/sren (simple re-enroll)
  */
-@SuppressWarnings("restriction")
 public class EstResource extends CoapResource {
 
     private static final Logger LOG = LoggerFactory.getLogger(EstResource.class);
@@ -98,63 +96,66 @@ public class EstResource extends CoapResource {
             exchange.respond(ResponseCode.BAD_REQUEST, "PKCS#10 payload mandatory");
             return;
         }
+
         // open the PCKS10 (Certificate Signing Request)
+        CertificationRequest csr = null;
         try {
-            PKCS10 csr = new PKCS10(request.getPayload());
-
-            String csrIdentity = csr.getSubjectName().getCommonName();
-
-            Map<String, Object> attributes = new HashMap<>();
-
-            for (PKCS10Attribute att : csr.getAttributes().getAttributes()) {
-                attributes.put(att.getAttributeId().toString(), att.getAttributeValue());
-            }
-            System.err.println(csrIdentity);
-            // looks at the DTLS session
-            Identity dtlsIdentity = ExchangeUtil.extractIdentity(exchange);
-
-            // try to find the end-point corresponding to the used DTLS credentials
-            String ep = "";
-
-            if (dtlsIdentity.isPSK()) {
-                SecurityInfo secInfo = bsSecurityStore.getByIdentity(dtlsIdentity.getPskIdentity());
-                if (secInfo == null) {
-                    exchange.respond(ResponseCode.BAD_REQUEST, "no endpoint associated with your DTLS identity");
-                    return;
-                }
-                ep = secInfo.getEndpoint();
-            } else if (dtlsIdentity.isRPK()) {
-                // not supported
-                exchange.respond(ResponseCode.INTERNAL_SERVER_ERROR, "DTLS RPK authentication not supported");
-                return;
-            } else if (dtlsIdentity.isX509()) {
-                // extract CN=
-                ep = dtlsIdentity.getX509CommonName();
-            } else {
-                exchange.respond(ResponseCode.BAD_REQUEST, "You must be authenticated using DTLS");
-                return;
-            }
-
-            // the CN in the CSR must match the end-point in the security store
-            if (!csrIdentity.equals(ep)) {
-                exchange.respond(ResponseCode.BAD_REQUEST, "Your CSR CN doesn't match your credentials");
-                return;
-            }
-
-            // enroll
-            byte[] x509 = pki.enroll(request.getPayload(), ep, attributes);
-            exchange.respond(ResponseCode.CONTENT, x509);
-        } catch (IOException e) {
-            LOG.error("Unexpected IOException", e);
+            csr = new CertificationRequest(ASN1Sequence.getInstance(request.getPayload()));
+        } catch (IllegalArgumentException e) {
+            LOG.error("Could not decode CSR", e);
             exchange.respond(ResponseCode.BAD_REQUEST, "Corrupted CSR");
             return;
-        } catch (SignatureException e) {
-            LOG.debug("Invalid PKCS10 signature", e);
-            exchange.respond(ResponseCode.BAD_REQUEST, "Invalid signature");
-            return;
-        } catch (NoSuchAlgorithmException e) {
-            LOG.warn("Unsupported PKCS10 signature", e);
-            exchange.respond(ResponseCode.BAD_REQUEST, "Unsupported algorithm");
         }
+
+        String csrIdentity = csr.getCertificationRequestInfo().getSubject().getRDNs(X509ObjectIdentifiers.commonName)[0]
+                .getFirst().getValue().toString();
+
+        Map<String, Object> attributes = new HashMap<>();
+
+        for (int i = 0; i < csr.getCertificationRequestInfo().getAttributes().size(); i++) {
+            ASN1Encodable asn1Att = csr.getCertificationRequestInfo().getAttributes().getObjectAt(i);
+            if (asn1Att instanceof Attribute) {
+                Attribute att = (Attribute) asn1Att;
+                // TODO do something
+                // attributes.put(att.getAttrType().getId(),
+                // att.getAttributeValues()[0].toASN1Primitive().getEncoded());
+            }
+        }
+
+        System.err.println(csrIdentity);
+        // looks at the DTLS session
+        Identity dtlsIdentity = ExchangeUtil.extractIdentity(exchange);
+
+        // try to find the end-point corresponding to the used DTLS credentials
+        String ep = "";
+
+        if (dtlsIdentity.isPSK()) {
+            SecurityInfo secInfo = bsSecurityStore.getByIdentity(dtlsIdentity.getPskIdentity());
+            if (secInfo == null) {
+                exchange.respond(ResponseCode.BAD_REQUEST, "no endpoint associated with your DTLS identity");
+                return;
+            }
+            ep = secInfo.getEndpoint();
+        } else if (dtlsIdentity.isRPK()) {
+            // not supported
+            exchange.respond(ResponseCode.INTERNAL_SERVER_ERROR, "DTLS RPK authentication not supported");
+            return;
+        } else if (dtlsIdentity.isX509()) {
+            // extract CN=
+            ep = dtlsIdentity.getX509CommonName();
+        } else {
+            exchange.respond(ResponseCode.BAD_REQUEST, "You must be authenticated using DTLS");
+            return;
+        }
+
+        // the CN in the CSR must match the end-point in the security store
+        if (!csrIdentity.equals(ep)) {
+            exchange.respond(ResponseCode.BAD_REQUEST, "Your CSR CN doesn't match your credentials");
+            return;
+        }
+
+        // enroll
+        byte[] x509 = pki.enroll(request.getPayload(), ep, attributes);
+        exchange.respond(ResponseCode.CONTENT, x509);
     }
 }
