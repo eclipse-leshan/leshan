16701bfcfe1e71ad64d8c266a341b1555d42fcb3
Daniel Persson <daniel.p.persson@husqvarnagroup.com>
2017-09-27 13:10:59 +0200

    Added ResponseCode.SERVICE_UNAVAILABLE
    Signed-off-by: Daniel Persson <daniel.p.persson@husqvarnagroup.com>

--- leshan-core-cf/src/main/java/org/eclipse/leshan/core/californium/ResponseCodeUtil.java
+++ leshan-core-cf/src/main/java/org/eclipse/leshan/core/californium/ResponseCodeUtil.java
@@ -1,15 +1,15 @@
 /*******************************************************************************
  * Copyright (c) 2017 Sierra Wireless and others.
- * 
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * and Eclipse Distribution License v1.0 which accompany this distribution.
- * 
+ *
  * The Eclipse Public License is available at
  *    http://www.eclipse.org/legal/epl-v10.html
  * and the Eclipse Distribution License is available at
  *    http://www.eclipse.org/org/documents/edl-v10.html.
- * 
+ *
  * Contributors:
  *     Sierra Wireless - initial API and implementation
  *******************************************************************************/
@@ -21,7 +21,7 @@ import org.eclipse.leshan.util.Validate;
 
 public class ResponseCodeUtil {
 
-    public static ResponseCode fromCoapCode(int code) {
+    public static ResponseCode fromCoapCode(final int code) {
         Validate.notNull(code);
 
         if (code == CoAP.ResponseCode.CREATED.value) {
@@ -48,37 +48,41 @@ public class ResponseCodeUtil {
             return ResponseCode.NOT_ACCEPTABLE;
         } else if (code == CoAP.ResponseCode.INTERNAL_SERVER_ERROR.value) {
             return ResponseCode.INTERNAL_SERVER_ERROR;
+        } else if (code == CoAP.ResponseCode.SERVICE_UNAVAILABLE.value) {
+            return ResponseCode.SERVICE_UNAVAILABLE;
         } else {
             throw new IllegalArgumentException("Invalid CoAP code for LWM2M response: " + code);
         }
     }
 
-    public static org.eclipse.californium.core.coap.CoAP.ResponseCode fromLwM2mCode(ResponseCode code) {
+    public static org.eclipse.californium.core.coap.CoAP.ResponseCode fromLwM2mCode(final ResponseCode code) {
         Validate.notNull(code);
 
         switch (code) {
-        case CREATED:
-            return org.eclipse.californium.core.coap.CoAP.ResponseCode.CREATED;
-        case DELETED:
-            return org.eclipse.californium.core.coap.CoAP.ResponseCode.DELETED;
-        case CHANGED:
-            return org.eclipse.californium.core.coap.CoAP.ResponseCode.CHANGED;
-        case CONTENT:
-            return org.eclipse.californium.core.coap.CoAP.ResponseCode.CONTENT;
-        case BAD_REQUEST:
-            return org.eclipse.californium.core.coap.CoAP.ResponseCode.BAD_REQUEST;
-        case UNAUTHORIZED:
-            return org.eclipse.californium.core.coap.CoAP.ResponseCode.UNAUTHORIZED;
-        case NOT_FOUND:
-            return org.eclipse.californium.core.coap.CoAP.ResponseCode.NOT_FOUND;
-        case METHOD_NOT_ALLOWED:
-            return org.eclipse.californium.core.coap.CoAP.ResponseCode.METHOD_NOT_ALLOWED;
-        case FORBIDDEN:
-            return org.eclipse.californium.core.coap.CoAP.ResponseCode.FORBIDDEN;
-        case INTERNAL_SERVER_ERROR:
-            return org.eclipse.californium.core.coap.CoAP.ResponseCode.INTERNAL_SERVER_ERROR;
-        default:
-            throw new IllegalArgumentException("Invalid CoAP code for LWM2M response: " + code);
+            case CREATED:
+                return org.eclipse.californium.core.coap.CoAP.ResponseCode.CREATED;
+            case DELETED:
+                return org.eclipse.californium.core.coap.CoAP.ResponseCode.DELETED;
+            case CHANGED:
+                return org.eclipse.californium.core.coap.CoAP.ResponseCode.CHANGED;
+            case CONTENT:
+                return org.eclipse.californium.core.coap.CoAP.ResponseCode.CONTENT;
+            case BAD_REQUEST:
+                return org.eclipse.californium.core.coap.CoAP.ResponseCode.BAD_REQUEST;
+            case UNAUTHORIZED:
+                return org.eclipse.californium.core.coap.CoAP.ResponseCode.UNAUTHORIZED;
+            case NOT_FOUND:
+                return org.eclipse.californium.core.coap.CoAP.ResponseCode.NOT_FOUND;
+            case METHOD_NOT_ALLOWED:
+                return org.eclipse.californium.core.coap.CoAP.ResponseCode.METHOD_NOT_ALLOWED;
+            case FORBIDDEN:
+                return org.eclipse.californium.core.coap.CoAP.ResponseCode.FORBIDDEN;
+            case INTERNAL_SERVER_ERROR:
+                return org.eclipse.californium.core.coap.CoAP.ResponseCode.INTERNAL_SERVER_ERROR;
+            case SERVICE_UNAVAILABLE:
+                return org.eclipse.californium.core.coap.CoAP.ResponseCode.SERVICE_UNAVAILABLE;
+            default:
+                throw new IllegalArgumentException("Invalid CoAP code for LWM2M response: " + code);
         }
     }
 }
--- leshan-core/src/main/java/org/eclipse/leshan/ResponseCode.java
+++ leshan-core/src/main/java/org/eclipse/leshan/ResponseCode.java
@@ -1,15 +1,15 @@
 /*******************************************************************************
  * Copyright (c) 2013-2015 Sierra Wireless and others.
- * 
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * and Eclipse Distribution License v1.0 which accompany this distribution.
- * 
+ *
  * The Eclipse Public License is available at
  *    http://www.eclipse.org/legal/epl-v10.html
  * and the Eclipse Distribution License is available at
  *    http://www.eclipse.org/org/documents/edl-v10.html.
- * 
+ *
  * Contributors:
  *     Sierra Wireless - initial API and implementation
  *******************************************************************************/
@@ -41,22 +41,25 @@ public enum ResponseCode {
     NOT_ACCEPTABLE,
     /** The specified format is not supported */
     UNSUPPORTED_CONTENT_FORMAT,
+    /** The requested service is unavailable */
+    SERVICE_UNAVAILABLE,
     /** generic response code for unexpected error */
     INTERNAL_SERVER_ERROR;
 
     public boolean isError() {
         switch (this) {
-        case UNAUTHORIZED:
-        case BAD_REQUEST:
-        case METHOD_NOT_ALLOWED:
-        case FORBIDDEN:
-        case NOT_FOUND:
-        case INTERNAL_SERVER_ERROR:
-        case UNSUPPORTED_CONTENT_FORMAT:
-        case NOT_ACCEPTABLE:
-            return true;
-        default:
-            return false;
+            case UNAUTHORIZED:
+            case BAD_REQUEST:
+            case METHOD_NOT_ALLOWED:
+            case FORBIDDEN:
+            case NOT_FOUND:
+            case INTERNAL_SERVER_ERROR:
+            case UNSUPPORTED_CONTENT_FORMAT:
+            case NOT_ACCEPTABLE:
+            case SERVICE_UNAVAILABLE:
+                return true;
+            default:
+                return false;
         }
     }
 }
