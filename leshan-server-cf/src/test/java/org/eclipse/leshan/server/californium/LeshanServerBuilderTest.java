cd08129dd633d725943bd8e1c4b4d1b515e807d6
Simon Bernard <sbernard@sierrawireless.com>
2017-09-27 16:18:32 +0200

    #392: fix builder and add some unit tests

---
+++ leshan-server-cf/src/test/java/org/eclipse/leshan/server/californium/LeshanServerBuilderTest.java
@@ -0,0 +1,73 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Sierra Wireless and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ * 
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ *    http://www.eclipse.org/org/documents/edl-v10.html.
+ * 
+ * Contributors:
+ *     Sierra Wireless - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.leshan.server.californium;
+
+import static org.junit.Assert.*;
+
+import org.eclipse.leshan.server.californium.impl.LeshanServer;
+import org.eclipse.leshan.server.impl.InMemorySecurityStore;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LeshanServerBuilderTest {
+
+    private LeshanServerBuilder builder;
+    private LeshanServer server;
+
+    @Before
+    public void start() {
+        builder = new LeshanServerBuilder();
+    }
+
+    @Test
+    public void create_server_without_any_parameter() {
+        server = builder.build();
+
+        assertNull(server.getSecuredAddress());
+        assertNotNull(server.getUnsecuredAddress());
+        assertNull(server.getSecurityStore());
+    }
+
+    @Test
+    public void create_server_with_securityStore() {
+        builder.setSecurityStore(new InMemorySecurityStore());
+        server = builder.build();
+
+        assertNotNull(server.getSecuredAddress());
+        assertNotNull(server.getUnsecuredAddress());
+        assertNotNull(server.getSecurityStore());
+    }
+
+    @Test
+    public void create_server_with_securityStore_and_disable_secured_endpoint() {
+        builder.setSecurityStore(new InMemorySecurityStore());
+        builder.disableSecuredEndpoint();
+        server = builder.build();
+
+        assertNull(server.getSecuredAddress());
+        assertNotNull(server.getUnsecuredAddress());
+    }
+
+    @Test
+    public void create_server_with_securityStore_and_disable_unsecured_endpoint() {
+        builder.setSecurityStore(new InMemorySecurityStore());
+        builder.disableUnsecuredEndpoint();
+        server = builder.build();
+
+        assertNotNull(server.getSecuredAddress());
+        assertNull(server.getUnsecuredAddress());
+    }
+}
