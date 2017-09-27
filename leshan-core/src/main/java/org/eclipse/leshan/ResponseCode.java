2ad93c9b78f5cb33d1d240b563d61662e27e097d
Daniel Persson <bighatsmallfeet@gmail.com>
2017-09-27 13:58:40 +0200

    Added ResponseCode.SERVICE_UNAVAILABLE
    Signed-off-by: Daniel Persson <bighatsmallfeet@gmail.com>

--- leshan-core/src/main/java/org/eclipse/leshan/ResponseCode.java
+++ leshan-core/src/main/java/org/eclipse/leshan/ResponseCode.java
@@ -1,15 +1,15 @@
 /*******************************************************************************
  * Copyright (c) 2013-2015 Sierra Wireless and others.
- * 
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * and Eclipse Distribution License v1.0 which accompany this distribution.
- * 
+ *
  * The Eclipse Public License is available at
  *    http://www.eclipse.org/legal/epl-v10.html
  * and the Eclipse Distribution License is available at
  *    http://www.eclipse.org/org/documents/edl-v10.html.
- * 
+ *
  * Contributors:
  *     Sierra Wireless - initial API and implementation
  *******************************************************************************/
@@ -42,7 +42,9 @@ public enum ResponseCode {
     /** The specified format is not supported */
     UNSUPPORTED_CONTENT_FORMAT,
     /** generic response code for unexpected error */
-    INTERNAL_SERVER_ERROR;
+    INTERNAL_SERVER_ERROR,
+    /** The requested service is unavailable */
+    SERVICE_UNAVAILABLE;
 
     public boolean isError() {
         switch (this) {
@@ -53,6 +55,7 @@ public enum ResponseCode {
         case NOT_FOUND:
         case INTERNAL_SERVER_ERROR:
         case UNSUPPORTED_CONTENT_FORMAT:
+        case SERVICE_UNAVAILABLE:
         case NOT_ACCEPTABLE:
             return true;
         default:
