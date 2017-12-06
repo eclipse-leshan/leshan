52e905b72e73c311217cba661d6f7e17ddc08dc6
Simon Bernard <sbernard@sierrawireless.com>
2017-12-06 16:39:32 +0100

    Clean observerSpec Test

--- leshan-core/src/test/java/org/eclipse/leshan/ObserveSpecTest.java
+++ leshan-core/src/test/java/org/eclipse/leshan/ObserveSpecTest.java
@@ -21,56 +21,49 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class ObserveSpecTest {
 
-    ObserveSpec spec;
-
-    @Before
-    public void setUp() throws Exception {
-    }
-
     @Test
     public void serialize_cancel_param() {
-        this.spec = new ObserveSpec.Builder().cancel().build();
-        Assert.assertEquals("cancel", this.spec.toQueryParams()[0]);
+        ObserveSpec spec = new ObserveSpec.Builder().cancel().build();
+        Assert.assertEquals("cancel", spec.toQueryParams()[0]);
     }
 
     @Test
     public void serialize_step_param() {
         Float step = 12.1f;
-        this.spec = new ObserveSpec.Builder().step(step).build();
-        Assert.assertEquals(String.format("st=%s", step), this.spec.toQueryParams()[0]);
+        ObserveSpec spec = new ObserveSpec.Builder().step(step).build();
+        Assert.assertEquals(String.format("st=%s", step), spec.toQueryParams()[0]);
     }
 
     @Test
     public void serialize_greaterThan_param() {
         Float threshold = 12.1f;
-        this.spec = new ObserveSpec.Builder().greaterThan(threshold).build();
-        Assert.assertEquals(String.format("gt=%s", threshold), this.spec.toQueryParams()[0]);
+        ObserveSpec spec = new ObserveSpec.Builder().greaterThan(threshold).build();
+        Assert.assertEquals(String.format("gt=%s", threshold), spec.toQueryParams()[0]);
     }
 
     @Test
     public void serialize_lessThan_param() {
         Float threshold = 12.1f;
-        this.spec = new ObserveSpec.Builder().lessThan(threshold).build();
-        Assert.assertEquals(String.format("lt=%s", threshold), this.spec.toQueryParams()[0]);
+        ObserveSpec spec = new ObserveSpec.Builder().lessThan(threshold).build();
+        Assert.assertEquals(String.format("lt=%s", threshold), spec.toQueryParams()[0]);
     }
 
     @Test
     public void serialize_minPeriod_param() {
         int seconds = 30;
-        this.spec = new ObserveSpec.Builder().minPeriod(seconds).build();
-        Assert.assertEquals(String.format("pmin=%s", seconds), this.spec.toQueryParams()[0]);
+        ObserveSpec spec = new ObserveSpec.Builder().minPeriod(seconds).build();
+        Assert.assertEquals(String.format("pmin=%s", seconds), spec.toQueryParams()[0]);
     }
 
     @Test
     public void serialize_maxPeriod_param() {
         int seconds = 60;
-        this.spec = new ObserveSpec.Builder().maxPeriod(seconds).build();
-        Assert.assertEquals(String.format("pmax=%s", seconds), this.spec.toQueryParams()[0]);
+        ObserveSpec spec = new ObserveSpec.Builder().maxPeriod(seconds).build();
+        Assert.assertEquals(String.format("pmax=%s", seconds), spec.toQueryParams()[0]);
     }
 
     @Test(expected = IllegalStateException.class)
