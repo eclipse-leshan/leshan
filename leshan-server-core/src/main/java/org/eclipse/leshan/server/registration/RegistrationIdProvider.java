0e814f4a2312b3d07f62d2c0b3f5e2e34480fa7c
Rokwoon Kim <k862390@gmail.com>
2018-04-27 13:09:23 +0900

    add RegistrationIdProvider to change default random string registrationId.
    Signed-off-by: Rokwoon Kim <k862390@gmail.com>

---
+++ leshan-server-core/src/main/java/org/eclipse/leshan/server/registration/RegistrationIdProvider.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2018 NTELS and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ *
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ *    http://www.eclipse.org/org/documents/edl-v10.html.
+ *
+ * Contributors:
+ *     Rokwoon Kim (contracted with NTELS) - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.leshan.server.registration;
+
+import org.eclipse.leshan.core.request.RegisterRequest;
+
+public interface RegistrationIdProvider {
+    
+    /**
+     * Returns the registrationId using for location-path option values on response of Register operation.
+     * 
+     * @param registerRequest the client's registration request information.
+     * @return registrationId
+     */
+    String getRegistrationId(RegisterRequest registerRequest);
+
+}
