b4159ed371926e3ab25c238fbf09f6cd76004b2e
Cavenaghi9 <zhangboya@gmail.com>
2019-02-12 17:18:05 +0800

    rename to LwM2M Node SenML Encoder/Decoder

--- leshan-client-demo/src/main/java/org/eclipse/leshan/client/demo/LeshanClientDemo.java
+++ leshan-client-demo/src/main/java/org/eclipse/leshan/client/demo/LeshanClientDemo.java
@@ -355,7 +355,7 @@ public class LeshanClientDemo {
                 initializer.setInstancesForObject(SERVER, new Server(123, 30, BindingMode.U, false));
             } else {
                 initializer.setInstancesForObject(SECURITY, noSec(serverURI, 123));
-                initializer.setInstancesForObject(SERVER, new Server(123, 30, BindingMode.U, false));
+                initializer.setInstancesForObject(SERVER, new Server(123, 3600, BindingMode.U, false));
             }
         }
         initializer.setClassForObject(DEVICE, MyDevice.class);
