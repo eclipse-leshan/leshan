77abfe9109e9ddccad481b0e5e15b457c7706607
Boya Zhang <zhangboya@gmail.com>
2019-03-04 14:27:55 +0800

    #659: Add SenML JSON encoder in leshan-core with unit tests.
    Signed-off-by: Boya Zhang <zhangboya@gmail.com>

---
+++ leshan-core/src/main/java/org/eclipse/leshan/senml/SenMLJson.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ * 
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ *    http://www.eclipse.org/org/documents/edl-v10.html.
+ * 
+ * Contributors:
+ *     Boya Zhang - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.leshan.senml;
+
+/**
+ * Helper for encoding/decoding SenML JSON format
+ */
+public class SenMLJson {
+    private static final SenMLPackSerDes serDes = new SenMLPackSerDes();
+
+    public static String toJsonSenML(SenMLPack pack) {
+        return serDes.serialize(pack);
+    }
+}
