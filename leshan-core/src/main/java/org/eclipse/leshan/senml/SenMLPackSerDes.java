983b21eedc0cd9a18f789124987cba7231f4ab83
Boya Zhang <zhangboya@gmail.com>
2019-04-03 11:28:59 +0800

    #673: SenML JSON decoder in leshan-core with unit tests.
    Signed-off-by: Boya Zhang <zhangboya@gmail.com>

--- leshan-core/src/main/java/org/eclipse/leshan/senml/SenMLPackSerDes.java
+++ leshan-core/src/main/java/org/eclipse/leshan/senml/SenMLPackSerDes.java
@@ -19,6 +19,7 @@ import org.eclipse.leshan.core.util.Base64;
 
 import com.eclipsesource.json.JsonArray;
 import com.eclipsesource.json.JsonObject;
+import com.eclipsesource.json.JsonValue;
 
 public class SenMLPackSerDes {
 
@@ -70,5 +71,59 @@ public class SenMLPackSerDes {
         }
 
         return jsonArray.toString();
+    }
+
+    public SenMLPack deserialize(JsonArray array) {
+
+        if (array == null)
+            return null;
+
+        SenMLPack pack = new SenMLPack();
+
+        for (JsonValue value : array.values()) {
+            JsonObject o = value.asObject();
+
+            SenMLRecord record = new SenMLRecord();
+
+            JsonValue bn = o.get("bn");
+            if (bn != null && bn.isString())
+                record.setBaseName(bn.asString());
+
+            JsonValue bt = o.get("bt");
+            if (bt != null && bt.isNumber())
+                record.setBaseTime(bt.asLong());
+
+            JsonValue n = o.get("n");
+            if (n != null && n.isString())
+                record.setName(n.asString());
+
+            JsonValue t = o.get("t");
+            if (t != null && t.isNumber())
+                record.setTime(t.asLong());
+
+            JsonValue v = o.get("v");
+            if (v != null && v.isNumber())
+                record.setFloatValue(v.asFloat());
+
+            JsonValue vb = o.get("vb");
+            if (vb != null && vb.isBoolean())
+                record.setBooleanValue(vb.asBoolean());
+
+            JsonValue vs = o.get("vs");
+            if (vs != null && vs.isString())
+                record.setStringValue(vs.asString());
+
+            JsonValue vlo = o.get("vlo");
+            if (vlo != null && vlo.isString())
+                record.setObjectLinkValue(vlo.asString());
+
+            JsonValue vd = o.get("vd");
+            if (vd != null && vd.isString())
+                record.setOpaqueValue(Base64.decodeBase64(vd.asString()));
+
+            pack.addRecord(record);
+        }
+
+        return pack;
     }
 }
