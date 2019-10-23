81436c433e303b3469a36d6a385a5c23bab34a0f
Simon Bernard <sbernard@sierrawireless.com>
2019-10-23 18:14:04 +0200

    Add some tests about start/stop/destroy servers

---
+++ leshan-server-cf/src/test/java/org/eclipse/leshan/server/californium/LeshanServerTest.java
@@ -0,0 +1,99 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Sierra Wireless and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ * 
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ *    http://www.eclipse.org/org/documents/edl-v10.html.
+ * 
+ * Contributors:
+ *     Sierra Wireless - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.leshan.server.californium;
+
+import static org.junit.Assert.assertEquals;
+
+import java.net.InetSocketAddress;
+
+import org.eclipse.leshan.core.request.BindingMode;
+import org.eclipse.leshan.core.request.Identity;
+import org.eclipse.leshan.core.request.ReadRequest;
+import org.eclipse.leshan.core.response.ErrorCallback;
+import org.eclipse.leshan.core.response.ReadResponse;
+import org.eclipse.leshan.core.response.ResponseCallback;
+import org.eclipse.leshan.server.queue.PresenceServiceImpl;
+import org.eclipse.leshan.server.registration.Registration;
+import org.junit.Test;
+
+public class LeshanServerTest {
+
+    @Test
+    public void testStartStopStart() throws InterruptedException {
+        LeshanServer server = new LeshanServerBuilder().build();
+
+        server.start();
+        Thread.sleep(100);
+        server.stop();
+        Thread.sleep(100);
+        server.start();
+    }
+
+    @Test
+    public void testStartDestroy() throws InterruptedException {
+        // look at nb active thread before.
+        int numberOfThreadbefore = Thread.activeCount();
+
+        LeshanServer server = new LeshanServerBuilder().build();
+        server.start();
+        Thread.sleep(100);
+        // HACK force creation thread creation.
+        forceThreadsCreation(server);
+        Thread.sleep(100);
+        server.destroy();
+
+        // ensure all thread are destroyed
+        Thread.sleep(500);
+        assertEquals("All news created threads must be destroyed", numberOfThreadbefore, Thread.activeCount());
+    }
+
+    @Test
+    public void testStartStopDestroy() throws InterruptedException {
+        // look at nb active thread before.
+        int numberOfThreadbefore = Thread.activeCount();
+
+        LeshanServer server = new LeshanServerBuilder().build();
+        server.start();
+        Thread.sleep(100);
+        // HACK force creation thread creation.
+        forceThreadsCreation(server);
+        Thread.sleep(100);
+        server.stop();
+        Thread.sleep(100);
+        server.destroy();
+
+        // ensure all thread are destroyed
+        Thread.sleep(500);
+        assertEquals("All news created threads must be destroyed", numberOfThreadbefore, Thread.activeCount());
+    }
+
+    private void forceThreadsCreation(LeshanServer server) {
+        Registration reg = new Registration.Builder("id", "endpoint", Identity.unsecure(new InetSocketAddress(5555)))
+                .bindingMode(BindingMode.UQ).build();
+        // Force timer thread creation of preference service.
+        ((PresenceServiceImpl) server.getPresenceService()).setAwake(reg);
+        // Force time thread creation of CoapAsyncRequestObserver
+        server.send(reg, new ReadRequest(3), new ResponseCallback<ReadResponse>() {
+            @Override
+            public void onResponse(ReadResponse response) {
+            }
+        }, new ErrorCallback() {
+            @Override
+            public void onError(Exception e) {
+            }
+        });
+    }
+}
---
+++ leshan-server-cf/src/test/java/org/eclipse/leshan/server/californium/bootstrap/LeshanBootstrapServerTest.java
@@ -0,0 +1,117 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Sierra Wireless and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ * 
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ *    http://www.eclipse.org/org/documents/edl-v10.html.
+ * 
+ * Contributors:
+ *     Sierra Wireless - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.leshan.server.californium.bootstrap;
+
+import static org.junit.Assert.assertEquals;
+
+import java.net.InetSocketAddress;
+
+import org.eclipse.leshan.core.request.BootstrapRequest;
+import org.eclipse.leshan.core.request.Identity;
+import org.eclipse.leshan.core.response.BootstrapResponse;
+import org.eclipse.leshan.core.response.SendableResponse;
+import org.eclipse.leshan.server.bootstrap.BootstrapConfig;
+import org.eclipse.leshan.server.bootstrap.BootstrapConfigStore;
+import org.eclipse.leshan.server.bootstrap.BootstrapHandler;
+import org.eclipse.leshan.server.bootstrap.BootstrapHandlerFactory;
+import org.eclipse.leshan.server.bootstrap.BootstrapSession;
+import org.eclipse.leshan.server.bootstrap.BootstrapSessionManager;
+import org.eclipse.leshan.server.bootstrap.DefaultBootstrapHandler;
+import org.eclipse.leshan.server.bootstrap.LwM2mBootstrapRequestSender;
+import org.junit.Test;
+
+public class LeshanBootstrapServerTest {
+
+    private BootstrapHandler bsHandler;
+
+    private LeshanBootstrapServer createBootstrapServer() {
+        LeshanBootstrapServerBuilder builder = new LeshanBootstrapServerBuilder();
+        builder.setBootstrapHandlerFactory(new BootstrapHandlerFactory() {
+
+            @Override
+            public BootstrapHandler create(BootstrapConfigStore store, LwM2mBootstrapRequestSender sender,
+                    BootstrapSessionManager sessionManager) {
+                bsHandler = new DefaultBootstrapHandler(store, sender, sessionManager);
+                return bsHandler;
+            }
+        });
+        builder.setConfigStore(new BootstrapConfigStore() {
+
+            @Override
+            public BootstrapConfig get(String endpoint, Identity deviceIdentity, BootstrapSession session) {
+                BootstrapConfig config = new BootstrapConfig();
+                config.toDelete.add("/");
+                return config;
+            }
+        });
+        return builder.build();
+    }
+
+    @Test
+    public void testStartStopStart() throws InterruptedException {
+        LeshanBootstrapServer server = createBootstrapServer();
+
+        server.start();
+        Thread.sleep(100);
+        server.stop();
+        Thread.sleep(100);
+        server.start();
+    }
+
+    @Test
+    public void testStartDestroy() throws InterruptedException {
+        // look at nb active thread before.
+        int numberOfThreadbefore = Thread.activeCount();
+
+        LeshanBootstrapServer server = createBootstrapServer();
+        server.start();
+        Thread.sleep(100);
+        // HACK force creation thread creation.
+        forceThreadsCreation();
+        Thread.sleep(100);
+        server.destroy();
+
+        // ensure all thread are destroyed
+        Thread.sleep(500);
+        assertEquals("All news created threads must be destroyed", numberOfThreadbefore, Thread.activeCount());
+    }
+
+    @Test
+    public void testStartStopDestroy() throws InterruptedException {
+        // look at nb active thread before.
+        int numberOfThreadbefore = Thread.activeCount();
+
+        LeshanBootstrapServer server = createBootstrapServer();
+        server.start();
+        Thread.sleep(100);
+        // HACK force creation thread creation.
+        forceThreadsCreation();
+        Thread.sleep(100);
+        server.stop();
+        Thread.sleep(100);
+        server.destroy();
+
+        // ensure all thread are destroyed
+        Thread.sleep(500);
+        assertEquals("All news created threads must be destroyed", numberOfThreadbefore, Thread.activeCount());
+    }
+
+    private void forceThreadsCreation() {
+        SendableResponse<BootstrapResponse> bootstrap = bsHandler
+                .bootstrap(Identity.unsecure(new InetSocketAddress(5683)), new BootstrapRequest("test"));
+        bootstrap.sent();
+    }
+}
