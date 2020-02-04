6aea664f62f9c756f75325208c6e1df4d3763232
Simon Bernard <sbernard@sierrawireless.com>
2020-02-04 16:33:25 +0100

    Rewrite some code to be compliant with android 4.4.2.

--- leshan-core/src/main/java/org/eclipse/leshan/core/credentials/CredentialsReader.java
+++ leshan-core/src/main/java/org/eclipse/leshan/core/credentials/CredentialsReader.java
@@ -19,8 +19,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.RandomAccessFile;
 import java.security.GeneralSecurityException;
 
 /**
@@ -33,8 +32,11 @@ public abstract class CredentialsReader<T> {
      * Read credential from file
      */
     public T readFromFile(String fileName) throws IOException, GeneralSecurityException {
-        byte[] bytes = Files.readAllBytes(Paths.get(fileName));
-        return decode(bytes);
+        try (RandomAccessFile f = new RandomAccessFile(fileName, "r")) {
+            byte[] bytes = new byte[(int) f.length()];
+            f.readFully(bytes);
+            return decode(bytes);
+        }
     }
 
     /**
