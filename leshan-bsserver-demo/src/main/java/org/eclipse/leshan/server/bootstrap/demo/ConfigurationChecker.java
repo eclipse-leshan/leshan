82bf4c16f92ea2ccea40473ad0259eeb612ada06
rikard-sics <rikard.hoglund@ri.se>
2020-07-01 16:44:47 +0200

    Removed unneeded assert checking the usingOscore boolean
    Signed-off-by: Rikard Höglund <rikard.hoglund@ri.se>

--- leshan-bsserver-demo/src/main/java/org/eclipse/leshan/server/bootstrap/demo/ConfigurationChecker.java
+++ leshan-bsserver-demo/src/main/java/org/eclipse/leshan/server/bootstrap/demo/ConfigurationChecker.java
@@ -65,7 +65,6 @@ public class ConfigurationChecker {
                 }
                 if (usingOscore) {
                     LOG.trace("Bootstrapping information contains OSCORE security object.");
-                    assertIf(usingOscore == false, "no oscore object found for bootstrap server security object");
                     assertIf(ArrayUtils.isEmpty(osc.oscoreMasterSecret), "master secret must not be empty");
                     assertIf(ArrayUtils.isEmpty(osc.oscoreSenderId) && ArrayUtils.isEmpty(osc.oscoreRecipientId),
                             "either sender ID or recipient ID must be filled");
