2ff39f91b3bf2a38b4df9ffb1ddfda1d23875711
Simon Bernard <sbernard@sierrawireless.com>
2020-12-02 15:03:39 +0100

    Add Node Encode/Decoder for CBOR Content Format

---
+++ leshan-core/src/main/java/org/eclipse/leshan/core/node/codec/cbor/LwM2mNodeCborDecoder.java
@@ -0,0 +1,168 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Sierra Wireless and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ * 
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v20.html
+ * and the Eclipse Distribution License is available at
+ *    http://www.eclipse.org/org/documents/edl-v10.html.
+ * 
+ * Contributors:
+ *     Sierra Wireless - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.leshan.core.node.codec.cbor;
+
+import java.util.Date;
+
+import org.eclipse.leshan.core.model.LwM2mModel;
+import org.eclipse.leshan.core.model.ResourceModel;
+import org.eclipse.leshan.core.model.ResourceModel.Type;
+import org.eclipse.leshan.core.node.LwM2mNode;
+import org.eclipse.leshan.core.node.LwM2mPath;
+import org.eclipse.leshan.core.node.LwM2mResourceInstance;
+import org.eclipse.leshan.core.node.LwM2mSingleResource;
+import org.eclipse.leshan.core.node.ObjectLink;
+import org.eclipse.leshan.core.node.codec.CodecException;
+import org.eclipse.leshan.core.node.codec.NodeDecoder;
+import org.eclipse.leshan.core.util.datatype.ULong;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.upokecenter.cbor.CBORException;
+import com.upokecenter.cbor.CBORNumber;
+import com.upokecenter.cbor.CBORObject;
+import com.upokecenter.cbor.CBORType;
+
+public class LwM2mNodeCborDecoder implements NodeDecoder {
+    private static final Logger LOG = LoggerFactory.getLogger(LwM2mNodeCborDecoder.class);
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T extends LwM2mNode> T decode(byte[] content, LwM2mPath path, LwM2mModel model, Class<T> nodeClass)
+            throws CodecException {
+
+        // Support only single value
+        if (!path.isResource() && !path.isResourceInstance())
+            throw new CodecException("Invalid path %s : CborDecoder decodes resource OR resource instance only", path);
+
+        if (content == null)
+            return null;
+
+        // Parse CBOR
+        CBORObject cborObject;
+        try {
+            cborObject = CBORObject.DecodeFromBytes(content);
+        } catch (CBORException e) {
+            throw new CodecException(e, "Unable to parse CBORD value %s for resource %s", content, path);
+        }
+
+        // Find model to know expected type
+        Type expectedType;
+        ResourceModel rDesc = model.getResourceModel(path.getObjectId(), path.getResourceId());
+        if (rDesc != null) {
+            expectedType = rDesc.type;
+        } else {
+            // try to guess type from CBOR ?
+            expectedType = guessTypeFromCbor(cborObject, path);
+            LOG.debug("Decoding CBOR resource or resource instance without model, type guessed {}", expectedType);
+        }
+
+        // Get Node Value
+        Object nodeValue = parseCborValue(cborObject, expectedType, path);
+
+        // Create Node
+        if (path.isResource()) {
+            return (T) LwM2mSingleResource.newResource(path.getResourceId(), nodeValue, expectedType);
+        } else {
+            return (T) LwM2mResourceInstance.newInstance(path.getResourceInstanceId(), nodeValue, expectedType);
+        }
+    }
+
+    private Type guessTypeFromCbor(CBORObject cborObject, LwM2mPath path) {
+        switch (cborObject.getType()) {
+        case Boolean:
+            return Type.BOOLEAN;
+        case FloatingPoint:
+            if (cborObject.HasTag(1))
+                return Type.TIME;
+            else
+                return Type.FLOAT;
+        case Integer:
+            if (cborObject.HasTag(1))
+                return Type.TIME;
+            else
+                return Type.FLOAT;
+        case TextString:
+            if (cborObject.HasTag(0))
+                return Type.TIME;
+            else
+                return Type.STRING;
+        case ByteString:
+            return Type.OPAQUE;
+        default:
+            throw new CodecException("Unable to guess LWM2M type for resource %s, cbor type is {}", path,
+                    cborObject.getType());
+        }
+    }
+
+    private Object parseCborValue(CBORObject cborObject, Type type, LwM2mPath path) throws CodecException {
+        LOG.trace("CBOR value for path {} and expected type {}: {}", path, type, cborObject.toString());
+
+        try {
+            if (cborObject.isNull())
+                return null;
+
+            switch (type) {
+            case STRING:
+                if (cborObject.getType() == CBORType.TextString) {
+                    return cborObject.AsString();
+                }
+                break;
+            case INTEGER:
+                if (cborObject.getType() == CBORType.Integer) {
+                    return cborObject.AsInt64Value();
+                }
+                break;
+            case UNSIGNED_INTEGER:
+                if (cborObject.getType() == CBORType.Integer) {
+                    CBORNumber number = cborObject.AsNumber();
+                    if (number.IsInteger() && !number.IsNegative()) {
+                        // There is maybe a better way to do this conversion ?
+                        return ULong.valueOf(cborObject.toString());
+                    }
+                }
+                break;
+            case BOOLEAN:
+                if (cborObject.getType() == CBORType.Boolean) {
+                    return cborObject.AsBoolean();
+                }
+            case FLOAT:
+                if (cborObject.getType() == CBORType.FloatingPoint) {
+                    return cborObject.AsDoubleValue();
+                }
+                break;
+            case TIME:
+                return cborObject.ToObject(Date.class);
+            case OBJLNK:
+                if (cborObject.getType() == CBORType.TextString) {
+                    return ObjectLink.decodeFromString(cborObject.AsString());
+                }
+            case OPAQUE:
+                if (cborObject.getType() == CBORType.ByteString) {
+                    return cborObject.GetByteString();
+                }
+                break;
+            default:
+                throw new CodecException("Unsupported type %s for resource %s", type, path);
+            }
+        } catch (IllegalStateException | ArithmeticException | NumberFormatException e) {
+            throw new CodecException(e, "Unable to convert CBOR value %s of type %s in type %s for resource %s",
+                    cborObject.toString(), cborObject.getType(), type, path);
+        }
+        throw new CodecException("Unable to convert CBOR value %s of type %s in type %s for resource %s",
+                cborObject.toString(), cborObject.getType(), type, path);
+    }
+}
---
+++ leshan-core/src/main/java/org/eclipse/leshan/core/node/codec/cbor/LwM2mNodeCborEncoder.java
@@ -0,0 +1,165 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Sierra Wireless and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ * 
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v20.html
+ * and the Eclipse Distribution License is available at
+ *    http://www.eclipse.org/org/documents/edl-v10.html.
+ * 
+ * Contributors:
+ *     Sierra Wireless - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.leshan.core.node.codec.cbor;
+
+import java.util.Date;
+
+import org.eclipse.leshan.core.model.LwM2mModel;
+import org.eclipse.leshan.core.model.ResourceModel;
+import org.eclipse.leshan.core.model.ResourceModel.Type;
+import org.eclipse.leshan.core.node.LwM2mNode;
+import org.eclipse.leshan.core.node.LwM2mNodeVisitor;
+import org.eclipse.leshan.core.node.LwM2mObject;
+import org.eclipse.leshan.core.node.LwM2mObjectInstance;
+import org.eclipse.leshan.core.node.LwM2mPath;
+import org.eclipse.leshan.core.node.LwM2mResource;
+import org.eclipse.leshan.core.node.LwM2mResourceInstance;
+import org.eclipse.leshan.core.node.ObjectLink;
+import org.eclipse.leshan.core.node.codec.CodecException;
+import org.eclipse.leshan.core.node.codec.LwM2mValueConverter;
+import org.eclipse.leshan.core.node.codec.NodeEncoder;
+import org.eclipse.leshan.core.util.Validate;
+import org.eclipse.leshan.core.util.datatype.ULong;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.upokecenter.cbor.CBORObject;
+import com.upokecenter.numbers.EInteger;
+
+public class LwM2mNodeCborEncoder implements NodeEncoder {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LwM2mNodeCborEncoder.class);
+
+    @Override
+    public byte[] encode(LwM2mNode node, LwM2mPath path, LwM2mModel model, LwM2mValueConverter converter)
+            throws CodecException {
+        Validate.notNull(node);
+        Validate.notNull(path);
+        Validate.notNull(model);
+
+        InternalEncoder internalEncoder = new InternalEncoder();
+        internalEncoder.path = path;
+        internalEncoder.model = model;
+        internalEncoder.converter = converter;
+        node.accept(internalEncoder);
+        return internalEncoder.encoded;
+    }
+
+    private static class InternalEncoder implements LwM2mNodeVisitor {
+        // visitor inputs
+        private LwM2mPath path;
+        private LwM2mModel model;
+        private LwM2mValueConverter converter;
+
+        // visitor output
+        private byte[] encoded = null;
+
+        @Override
+        public void visit(LwM2mObject object) {
+            throw new CodecException("Object %s cannot be encoded in cbor format", path);
+        }
+
+        @Override
+        public void visit(LwM2mObjectInstance instance) {
+            throw new CodecException("Object instance %s cannot be encoded in cbor format", path);
+        }
+
+        @Override
+        public void visit(LwM2mResource resource) {
+            if (resource.isMultiInstances()) {
+                throw new CodecException("Multiple instances resource %s cannot be encoded in cbor format", path);
+            }
+            LOG.trace("Encoding resource {} into cbor", resource);
+
+            ResourceModel rSpec = model.getResourceModel(path.getObjectId(), resource.getId());
+            Type expectedType = rSpec != null ? rSpec.type : resource.getType();
+            Object val = converter.convertValue(resource.getValue(), resource.getType(), expectedType, path);
+
+            if (expectedType == null) {
+                throw new CodecException(
+                        "Unable to encode value for resource {} without type(probably a executable one)", path);
+            }
+
+            CBORObject cbor = getCborValue(expectedType, val);
+
+            encoded = cbor.EncodeToBytes();
+        }
+
+        @Override
+        public void visit(LwM2mResourceInstance instance) {
+            LOG.trace("Encoding resource instance {} into cbor", instance);
+
+            ResourceModel rSpec = model.getResourceModel(path.getObjectId(), path.getResourceId());
+            Type expectedType = rSpec != null ? rSpec.type : instance.getType();
+            Object val = converter.convertValue(instance.getValue(), instance.getType(), expectedType, path);
+
+            if (expectedType == null) {
+                throw new CodecException(
+                        "Unable to encode value for resource {} without type(probably a executable one)", path);
+            }
+
+            CBORObject cbor = getCborValue(expectedType, val);
+
+            encoded = cbor.EncodeToBytes();
+        }
+
+        private CBORObject getCborValue(Type expectedType, Object val) {
+            CBORObject cbor;
+            if (val == null) {
+                return CBORObject.Null;
+            }
+            switch (expectedType) {
+            case INTEGER:
+                cbor = CBORObject.FromObject((long) val);
+                break;
+            case FLOAT:
+                cbor = CBORObject.FromObject((double) val);
+                break;
+            case STRING:
+                cbor = CBORObject.FromObject((String) val);
+                break;
+            case UNSIGNED_INTEGER:
+                cbor = CBORObject.FromObject(EInteger.FromString(((ULong) val).toString()));
+                break;
+            case BOOLEAN:
+                cbor = ((Boolean) val) ? CBORObject.True : CBORObject.False;
+                break;
+            case TIME:
+                // see https://tools.ietf.org/html/rfc7049#section-2.4.1
+                // number of seconds since 1970/1/1
+                long time = ((Date) val).getTime() / 1000;
+                cbor = CBORObject.FromObjectAndTag(time, 1);
+
+                // maybe a new API will be available in new version of CBOR-java :
+                // https://github.com/peteroupc/CBOR-Java/issues/14#issuecomment-737114305
+
+                // using this could works but encode the date as string sounds a bit less straightforward.
+                // cbor = CBORObject.FromObject(val); // val must be a Date
+                break;
+            case OBJLNK:
+                ObjectLink objlnk = (ObjectLink) val;
+                cbor = CBORObject.FromObject(objlnk.encodeToString());
+                break;
+            case OPAQUE:
+                cbor = CBORObject.FromObject((byte[]) val);
+                break;
+            default:
+                throw new CodecException("Cannot encode %s in cbor format for %s", val, path);
+            }
+            return cbor;
+        }
+    }
+}
