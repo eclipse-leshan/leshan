31e29703a80e43b50f934d2dd6cadb92c4aa357a
moznion <moznion@gmail.com>
2020-12-09 02:11:30 +0900

    #933: Add new interfaces to core: Destroyable, Startable and Stoppable
    Signed-off-by: moznion <moznion@gmail.com>

---
+++ leshan-core/src/main/java/org/eclipse/leshan/core/Destroyable.java
@@ -0,0 +1,6 @@
+package org.eclipse.leshan.core;
+
+public interface Destroyable {
+	/** Destroy the instances and frees all system resources. */
+	void destroy();
+}
---
+++ leshan-core/src/main/java/org/eclipse/leshan/core/Startable.java
@@ -0,0 +1,5 @@
+package org.eclipse.leshan.core;
+
+public interface Startable {
+	void start();
+}
