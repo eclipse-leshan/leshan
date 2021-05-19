de609c1e6a8eb63424013cf77ef228f799b3c15d
Simon Bernard <sbernard@sierrawireless.com>
2021-05-19 11:06:59 +0200

    Server-demo-v2: add more object icons.

--- leshan-server-demo/webapp2/src/components/ObjectIcon.vue
+++ leshan-server-demo/webapp2/src/components/ObjectIcon.vue
@@ -7,12 +7,30 @@ export default {
   methods: {
     idToIcon(id) {
       switch (id) {
-        case 1:
+        case 1: // server
           return "mdi-server";
-        case 3:
+        case 2: // ACL
+          return "mdi-shield-check-outline";
+        case 3: // device
           return "mdi-devices";
-        case 6:
+        case 4: // connectivity monitoring
+          return "mdi-signal-variant";
+        case 5: // firmware update
+          return "mdi-cellphone-arrow-down";
+        case 6: // location
           return "mdi-map-marker";
+        case 7: // connectivity statistics
+          return "mdi-signal-distance-variant";
+        case 11: // LWM2M APN Connection Profile
+          return "mdi-sim";
+        case 14: // LWM2M Software Component
+          return "mdi-puzzle-outline";
+        case 15: // DevCapMgmt
+          return "mdi-widgets-outline";
+        case 16: // Portfolio
+          return "mdi-folder-multiple-outline";
+        case 19: // BinaryAppDataContainer
+          return "mdi-database-sync-outline";
         case 3303:
           return "mdi-thermometer";
         default:
