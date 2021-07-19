543d8e25e06a0f087754e8c41695c2d4d8d05ab7
Simon Bernard <sbernard@sierrawireless.com>
2021-07-19 15:28:38 +0200

    Improve default implementation of BaseInstanceEnabler.write()

--- leshan-client-core/src/main/java/org/eclipse/leshan/client/resource/BaseInstanceEnabler.java
+++ leshan-client-core/src/main/java/org/eclipse/leshan/client/resource/BaseInstanceEnabler.java
@@ -28,7 +28,6 @@ import org.eclipse.leshan.client.servers.ServerIdentity;
 import org.eclipse.leshan.core.model.ObjectModel;
 import org.eclipse.leshan.core.model.ResourceModel;
 import org.eclipse.leshan.core.node.LwM2mMultipleResource;
-import org.eclipse.leshan.core.node.LwM2mNode;
 import org.eclipse.leshan.core.node.LwM2mObjectInstance;
 import org.eclipse.leshan.core.node.LwM2mResource;
 import org.eclipse.leshan.core.node.LwM2mResourceInstance;
@@ -174,21 +173,15 @@ public class BaseInstanceEnabler implements LwM2mInstanceEnabler {
     @Override
     public WriteResponse write(ServerIdentity identity, boolean addIfAbsent, int resourceid, int resourceInstanceId,
             LwM2mResourceInstance value) {
+
         // this is a sub-optimal default implementation
-        ReadResponse response = read(ServerIdentity.SYSTEM, resourceid);
-        if (response.isSuccess()) {
-            LwM2mNode content = response.getContent();
-            if (content instanceof LwM2mMultipleResource) {
-                LwM2mMultipleResource multiresource = ((LwM2mMultipleResource) content);
-                Map<Integer, LwM2mResourceInstance> instances = new HashMap<>(multiresource.getInstances());
-                if (addIfAbsent || instances.containsKey(resourceInstanceId)) {
-                    instances.put(resourceInstanceId, value);
-                    return write(identity, true, resourceid,
-                            new LwM2mMultipleResource(resourceid, value.getType(), instances.values()));
-                }
+        if (!addIfAbsent) {
+            ReadResponse response = read(ServerIdentity.SYSTEM, resourceid, resourceInstanceId);
+            if (!response.isSuccess()) {
+                return WriteResponse.notFound();
             }
         }
-        return WriteResponse.notFound();
+        return write(identity, false, resourceid, new LwM2mMultipleResource(resourceid, value.getType(), value));
     }
 
     @Override
