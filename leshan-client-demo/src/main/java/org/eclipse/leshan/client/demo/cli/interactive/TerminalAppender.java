346687ae98054d6d3ea1144a86fdf1efff9d704c
Simon Bernard <sbernard@sierrawireless.com>
2021-07-29 18:25:20 +0200

    Implement the InteractiveCLI using Jline3 and picocli

--- leshan-client-demo/src/main/java/org/eclipse/leshan/client/demo/cli/interactive/TerminalAppender.java
+++ leshan-client-demo/src/main/java/org/eclipse/leshan/client/demo/cli/interactive/TerminalAppender.java
@@ -1,46 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Sierra Wireless and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ * 
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v20.html
+ * and the Eclipse Distribution License is available at
+ *    http://www.eclipse.org/org/documents/edl-v10.html.
+ * 
+ * Contributors:
+ *     Sierra Wireless - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.leshan.client.demo.cli.interactive;
 
-import java.io.IOException;
+import org.jline.reader.LineReader;
 
 import ch.qos.logback.core.ConsoleAppender;
-import jline.console.ConsoleReader;
 
-/**
- * A logback Console appender compatible with a Jline 2 Console reader.
- */
 public class TerminalAppender<E> extends ConsoleAppender<E> {
 
-    private ConsoleReader console;
-    private String prompt;
+    private LineReader reader;
 
-    @Override
-    protected void subAppend(E event) {
-        if (console == null || !console.getTerminal().isAnsiSupported())
-            super.subAppend(event);
-        else {
-            // stash prompt
-            String stashed = "";
-            try {
-                stashed = console.getCursorBuffer().copy().toString();
-                console.resetPromptLine("", "", -1);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            // Display logs
-            super.subAppend(event);
-
-            // unstash prompt
-            try {
-                console.resetPromptLine(prompt, stashed, -1);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+    public void setReader(LineReader reader) {
+        this.reader = reader;
     }
 
-    public void setConsole(ConsoleReader console) {
-        this.console = console;
-        this.prompt = console.getPrompt();
+    @Override
+    protected void append(E eventObject) {
+        if (reader == null) {
+            super.append(eventObject);
+        } else {
+            reader.printAbove(new String(getEncoder().encode(eventObject)));
+        }
     }
 }
