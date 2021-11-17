e1014e9d851cb2d4fe30c333c36ac9d5cc4e99f0
Simon Bernard <sbernard@sierrawireless.com>
2021-11-17 10:10:47 +0100

    Add "send Data" support to LwM2mTestObject

--- leshan-client-core/src/main/java/org/eclipse/leshan/client/object/LwM2mTestObject.java
+++ leshan-client-core/src/main/java/org/eclipse/leshan/client/object/LwM2mTestObject.java
@@ -15,28 +15,39 @@
  *******************************************************************************/
 package org.eclipse.leshan.client.object;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
 import org.eclipse.leshan.client.resource.SimpleInstanceEnabler;
 import org.eclipse.leshan.client.servers.ServerIdentity;
+import org.eclipse.leshan.core.node.LwM2mPath;
+import org.eclipse.leshan.core.node.LwM2mResource;
 import org.eclipse.leshan.core.node.LwM2mResourceInstance;
 import org.eclipse.leshan.core.node.ObjectLink;
+import org.eclipse.leshan.core.request.ContentFormat;
 import org.eclipse.leshan.core.request.argument.Argument;
 import org.eclipse.leshan.core.request.argument.Arguments;
+import org.eclipse.leshan.core.response.ErrorCallback;
 import org.eclipse.leshan.core.response.ExecuteResponse;
+import org.eclipse.leshan.core.response.ResponseCallback;
+import org.eclipse.leshan.core.response.SendResponse;
 import org.eclipse.leshan.core.util.Hex;
 import org.eclipse.leshan.core.util.RandomStringUtils;
 import org.eclipse.leshan.core.util.datatype.ULong;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * This aims to implements LWM2M Test Object(Id:3441) from LWM2M registry.
  */
 public class LwM2mTestObject extends SimpleInstanceEnabler {
+    private static final Logger LOG = LoggerFactory.getLogger(LwM2mTestObject.class);
 
     private Random random = new Random(System.currentTimeMillis());
 
@@ -45,6 +56,7 @@ public class LwM2mTestObject extends SimpleInstanceEnabler {
         initialValues = new HashMap<>();
 
         initialValues.put(4, Collections.EMPTY_MAP);
+        initialValues.put(6, Collections.EMPTY_MAP);
 
         // single
         initialValues.put(110, "initial value");
@@ -73,6 +85,7 @@ public class LwM2mTestObject extends SimpleInstanceEnabler {
         Map<Integer, Object> clearedValues = new HashMap<>();
 
         clearedValues.put(4, Collections.EMPTY_MAP);
+        clearedValues.put(6, Collections.EMPTY_MAP);
 
         // single
         clearedValues.put(110, "");
@@ -142,6 +155,65 @@ public class LwM2mTestObject extends SimpleInstanceEnabler {
         fireResourcesChange(applyValues(newParams));
     }
 
+    private ExecuteResponse sendData(ServerIdentity server, Arguments arguments) {
+        // get path of resources to send
+        List<String> paths = new ArrayList<>();
+        LwM2mResource lwM2mResource = resources.get(6);
+        if (lwM2mResource != null && lwM2mResource.isMultiInstances()) {
+            for (LwM2mResourceInstance instance : lwM2mResource.getInstances().values()) {
+                try {
+                    paths.add(new LwM2mPath(instance.getValue().toString()).toString());
+                } catch (IllegalArgumentException e) {
+                    LOG.warn("Instance {} of resource 6 contains invalid path", instance.getId());
+                }
+            }
+        }
+        if (paths.isEmpty()) {
+            LOG.info("Unable to Send Data to {} as resources 6 {} doesn't contain any valid LWM2M path", server,
+                    lwM2mResource);
+            return ExecuteResponse.internalServerError("resources 6 doesn't contain any valid LWM2M path");
+        }
+
+        ContentFormat format = ContentFormat.SENML_CBOR;
+        Argument contenFormatArg = arguments.get(0);
+        try {
+            if (contenFormatArg != null)
+                format = ContentFormat.fromCode(contenFormatArg.getValue());
+        } catch (NumberFormatException e) {
+            return ExecuteResponse.badRequest(
+                    String.format("argument 1 %s is not a valid ContentFormat.", contenFormatArg.getValue()));
+        }
+        long timeout = 3000;// ms
+        Argument timeoutArg = arguments.get(1);
+        try {
+            if (timeoutArg != null)
+                timeout = Long.parseLong(timeoutArg.getValue());
+        } catch (NumberFormatException e) {
+            return ExecuteResponse
+                    .badRequest(String.format("argument 0 %s is not a valid timeout.", timeoutArg.getValue()));
+        }
+
+        // send data;
+        LOG.info("Try to send data {} using {} to {} (timeout {}ms)...", paths, format, server, timeout);
+        getLwM2mClient().sendData(server, format, paths, timeout, new ResponseCallback<SendResponse>() {
+            @Override
+            public void onResponse(SendResponse response) {
+                if (response.isSuccess()) {
+                    LOG.info("... Send Succeed {}", response.getCode());
+                } else {
+                    LOG.warn("... Send Response : {} {}", response.getCode(), response.getErrorMessage());
+                }
+
+            }
+        }, new ErrorCallback() {
+            @Override
+            public void onError(Exception e) {
+                LOG.error("... Send failed.", e);
+            }
+        });
+        return ExecuteResponse.success();
+    }
+
     private Map<Integer, ?> generateResourceInstances(ValueGenerator<?> generator) {
         HashMap<Integer, Object> instances = new HashMap<>();
         for (int i = 0; i < random.nextInt(10); i++) {
@@ -165,6 +237,8 @@ public class LwM2mTestObject extends SimpleInstanceEnabler {
         case 3:
             storeArguments(arguments);
             return ExecuteResponse.success();
+        case 5:
+            return sendData(identity, arguments);
         default:
             return super.execute(identity, resourceid, arguments);
         }
