e2e9539fb36eeb0f7020d519fd238051cb9ed0b1
Simon Bernard <sbernard@sierrawireless.com>
2021-12-02 17:03:22 +0100

    fix warning

--- leshan-core/src/main/java/org/eclipse/leshan/core/request/argument/InvalidArgumentException.java
+++ leshan-core/src/main/java/org/eclipse/leshan/core/request/argument/InvalidArgumentException.java
@@ -20,6 +20,8 @@ package org.eclipse.leshan.core.request.argument;
  */
 public class InvalidArgumentException extends Exception {
 
+    private static final long serialVersionUID = 1L;
+
     public InvalidArgumentException(String message) {
         super(message);
     }
