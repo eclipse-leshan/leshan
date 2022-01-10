eclipse-leshan/leshan

commit c30fe46b1c36e999c1755ab4dc9cf7b5431aae74

server-demo: Add Observe-Composite Operation to leshan-server-demo

leshan-server-demo/src/main/java/org/eclipse/leshan/server/demo/servlet/EventServlet.java
@@ -18,8 +18,10 @@
 package org.eclipse.leshan.server.demo.servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
@@ -158,49 +160,59 @@ public class EventServlet extends EventSourceServlet {
 
         @Override
         public void onResponse(SingleObservation observation, Registration registration, ObserveResponse response) {
-            String path = getObservationPaths(observation);
-            LwM2mNode content = response.getContent();
-            String stringContent = content.toString();
-            String jsonContent = null;
-            try {
-                jsonContent = mapper.writeValueAsString(content);
-            } catch (JsonProcessingException e) {
-                throw new RuntimeException(e);
-            }
-
-            onResponseCommon(registration, path, stringContent, jsonContent);
-        }
-
-        @Override
-        public void onResponse(CompositeObservation observation, Registration registration,
-                ObserveCompositeResponse response) {
-            String path = getObservationPaths(observation);
-
-            Map<LwM2mPath, LwM2mNode> content = response.getContent();
-            String stringContent = content.toString();
-            String jsonContent = null;
-            try {
-                jsonContent = mapper.writeValueAsString(content);
-            } catch (JsonProcessingException e) {
-                throw new RuntimeException(e);
-            }
-
-            onResponseCommon(registration, path, stringContent, jsonContent);
-        }
-
-        private void onResponseCommon(Registration registration, String path, String stringContent,
-                String jsonContent) {
 
             if (LOG.isDebugEnabled()) {
-                LOG.debug("Received notification from [{}] containing value [{}]", path, stringContent);
+                LOG.debug("Received notification from [{}] containing value [{}]", observation.getPath(),
+                        response.getContent());
+            }
+            String jsonContent = null;
+            try {
+                jsonContent = mapper.writeValueAsString(response.getContent());
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
             }
 
             if (registration != null) {
                 String data = new StringBuilder("{\"ep\":\"") //
                         .append(registration.getEndpoint()) //
+                        .append("\",\"kind\":\"single\"") //
                         .append("\",\"res\":\"") //
-                        .append(path).append("\",\"val\":") //
+                        .append(observation.getPath()).append("\",\"val\":") //
                         .append(jsonContent) //
+                        .append("}") //
+                        .toString();
+
+                sendEvent(EVENT_NOTIFICATION, data, registration.getEndpoint());
+            }
+        }
+
+        @Override
+        public void onResponse(CompositeObservation observation, Registration registration,
+                ObserveCompositeResponse response) {
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Received composite notificationfrom [{}] containing value [{}]", response.getContent());
+            }
+            String jsonContent = null;
+            String jsonListOfPath = null;
+            try {
+                jsonContent = mapper.writeValueAsString(response.getContent());
+                List<String> paths = new ArrayList<String>();
+                for (LwM2mPath path : response.getObservation().getPaths()) {
+                    paths.add(path.toString());
+                }
+                jsonListOfPath = mapper.writeValueAsString(paths);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+
+            if (registration != null) {
+                String data = new StringBuilder("{\"ep\":\"") //
+                        .append(registration.getEndpoint()) //
+                        .append("\",\"kind\":\"composite\"") //
+                        .append(",\"val\":") //
+                        .append(jsonContent) //
+                        .append(",\"paths\":") //
+                        .append(jsonListOfPath) //
                         .append("}") //
                         .toString();
 

leshan-server-demo/src/main/java/org/eclipse/leshan/server/demo/servlet/ClientServlet.java
@@ -42,6 +42,7 @@ import org.eclipse.leshan.core.request.CreateRequest;
 import org.eclipse.leshan.core.request.DeleteRequest;
 import org.eclipse.leshan.core.request.DiscoverRequest;
 import org.eclipse.leshan.core.request.ExecuteRequest;
+import org.eclipse.leshan.core.request.ObserveCompositeRequest;
 import org.eclipse.leshan.core.request.ObserveRequest;
 import org.eclipse.leshan.core.request.ReadCompositeRequest;
 import org.eclipse.leshan.core.request.ReadRequest;
@@ -58,6 +59,7 @@ import org.eclipse.leshan.core.response.DeleteResponse;
 import org.eclipse.leshan.core.response.DiscoverResponse;
 import org.eclipse.leshan.core.response.ExecuteResponse;
 import org.eclipse.leshan.core.response.LwM2mResponse;
+import org.eclipse.leshan.core.response.ObserveCompositeResponse;
 import org.eclipse.leshan.core.response.ObserveResponse;
 import org.eclipse.leshan.core.response.ReadCompositeResponse;
 import org.eclipse.leshan.core.response.ReadResponse;
@@ -325,6 +327,40 @@ public class ClientServlet extends HttpServlet {
         String[] path = StringUtils.split(req.getPathInfo(), '/');
         String clientEndpoint = path[0];
 
+        // /clients/endPoint/composite/observe : do LightWeight M2M observe request on a given client.
+        if (path.length == 3 && "composite".equals(path[1]) && "observe".equals(path[2])) {
+            try {
+                Registration registration = server.getRegistrationService().getByEndpoint(clientEndpoint);
+                if (registration != null) {
+                    // get paths
+                    String pathParam = req.getParameter(PATH_PARAM);
+                    String[] paths = pathParam.split(",");
+
+                    // get content format
+                    String pathContentFormatParam = req.getParameter(PATH_FORMAT_PARAM);
+                    ContentFormat pathContentFormat = pathContentFormatParam != null
+                            ? ContentFormat.fromName(pathContentFormatParam.toUpperCase())
+                            : null;
+                    String nodeContentFormatParam = req.getParameter(NODE_FORMAT_PARAM);
+                    ContentFormat nodeContentFormat = nodeContentFormatParam != null
+                            ? ContentFormat.fromName(nodeContentFormatParam.toUpperCase())
+                            : null;
+
+                    // create & process request
+                    ObserveCompositeRequest request = new ObserveCompositeRequest(pathContentFormat, nodeContentFormat,
+                            paths);
+                    ObserveCompositeResponse cResponse = server.send(registration, request, extractTimeout(req));
+                    processDeviceResponse(req, resp, cResponse);
+                } else {
+                    resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                    resp.getWriter().format("No registered client with id '%s'", clientEndpoint).flush();
+                }
+            } catch (RuntimeException | InterruptedException e) {
+                handleException(e, resp);
+            }
+            return;
+        }
+
         // /clients/endPoint/LWRequest/observe : do LightWeight M2M observe request on a given client.
         if (path.length >= 3 && "observe".equals(path[path.length - 1])) {
             try {
@@ -417,6 +453,27 @@ public class ClientServlet extends HttpServlet {
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String[] path = StringUtils.split(req.getPathInfo(), '/');
         String clientEndpoint = path[0];
+
+        // /clients/endPoint/composite/observe : do LightWeight M2M observe request on a given client.
+        if (path.length == 3 && "composite".equals(path[1]) && "observe".equals(path[2])) {
+            try {
+                Registration registration = server.getRegistrationService().getByEndpoint(clientEndpoint);
+                if (registration != null) {
+                    // get paths
+                    String pathParam = req.getParameter(PATH_PARAM);
+                    String[] paths = pathParam.split(",");
+
+                    server.getObservationService().cancelCompositeObservations(registration, paths);
+                    resp.setStatus(HttpServletResponse.SC_OK);
+                } else {
+                    resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                    resp.getWriter().format("no registered client with id '%s'", clientEndpoint).flush();
+                }
+            } catch (RuntimeException e) {
+                handleException(e, resp);
+            }
+            return;
+        }
 
         // /clients/endPoint/LWRequest/observe : cancel observation for the given resource.
         if (path.length >= 3 && "observe".equals(path[path.length - 1])) {

leshan-server-demo/webapp/src/components/compositeOperation/CompositeObjectControl.vue
@@ -13,6 +13,12 @@
 <template>
   <span>
     <request-button @on-click="read" title="Composite Read">R</request-button>
+    <request-button @on-click="observe" title="Composite Observe"
+      >Obs</request-button
+    >
+    <request-button @on-click="stopObserve" title="Passive Cancel Obverse">
+      <v-icon dense small>mdi-eye-remove-outline</v-icon></request-button
+    >
     <composite-operation-setting-menu>
       <template v-slot:activator="{ on, attrs }">
         <v-btn
@@ -94,6 +100,40 @@ export default {
           if (response.data.success) {
             this.$store.newNodes(this.endpoint, response.data.content);
           }
+        })
+        .catch(() => {
+          requestButton.resetState();
+        });
+    },
+    observe(requestButton) {
+      this.axios
+        .post(
+          `${this.requestPath()}/observe${this.requestOption()}&paths=${this.compositeObject.paths.join(
+            ","
+          )}`
+        )
+        .then((response) => {
+          this.updateState(response.data, requestButton);
+          if (response.data.success) {
+            this.$store.newNodes(this.endpoint, response.data.content);
+            this.$store.setCompositeObjectObserved(this.endpoint, this.compositeObject, true);
+          }
+        })
+        .catch(() => {
+          requestButton.resetState();
+        });
+    },
+    stopObserve(requestButton) {
+      this.axios  
+        .delete(
+          `${this.requestPath()}/observe?paths=${this.compositeObject.paths.join(
+            ","
+          )}`
+        )
+        .then(() => {
+          requestButton.changeState("success");
+          this.$store.setObserved(this.endpoint, this.path, false);
+          this.$store.setCompositeObjectObserved(this.endpoint, this.compositeObject, false);
         })
         .catch(() => {
           requestButton.resetState();

leshan-server-demo/webapp/src/plugins/store.js
@@ -28,6 +28,7 @@ class Store {
                                                 },    
              },
       observed : { path(object/instance/resource/resourceinstance) : boolean }
+      compositeObserved : { 'path1,path2,...' : boolean}
       }
     };  
    */
@@ -45,6 +46,7 @@ class Store {
     Vue.set(this.state, endpoint, {
       data: {},
       observed: {},
+      compositeObserved: {},
     });
   }
 
@@ -210,12 +212,7 @@ class Store {
     if (node.kind === "singleResource") {
       this.newResourceValue(endpoint, path, node, supposed);
     } else if (node.kind === "multiResource") {
-      this.newMultiResourceValue(
-        endpoint,
-        path,
-        node['values'],
-        supposed
-      );
+      this.newMultiResourceValue(endpoint, path, node["values"], supposed);
     } else if (node.kind === "resourceInstance") {
       this.newResourceInstanceValueFromPath(
         endpoint,
@@ -278,6 +275,38 @@ class Store {
     } else {
       this.state[endpoint].observed[path] = observed;
     }
+  }
+
+  /**
+   * @param {String} endpoint endpoint of the client
+   * @param {Array} nodes the nodes map {path:node Object}
+   * @param {Boolean} supposed true means the paths are "composite" observed.
+   */
+  setCompositePathsObserved(endpoint, paths, observed) {
+    let key = paths.join(",");
+    let o = this.state[endpoint].compositeObserved[key];
+    if (!o) {
+      Vue.set(this.state[endpoint].compositeObserved, key, observed);
+    } else {
+      this.state[endpoint].compositeObserved[key] = observed;
+    }
+  }
+
+  /**
+   * @param {Object} compositeObject
+   * @returns the key used in state.compositeObserved for the given compositeObject
+   */
+  compositeObjectToKey(compositeObject) {
+    return compositeObject.paths.join(",");
+  }
+
+  /**
+   * @param {String} endpoint endpoint of the client
+   * @param {Object} compositeObject A composite object
+   * @param {Boolean} supposed true means the paths are "composite" observed.
+   */
+  setCompositeObjectObserved(endpoint, compositeObject, observed) {
+    this.setCompositePathsObserved(endpoint, compositeObject.paths, observed);
   }
 }
 

leshan-server-demo/webapp/src/views/Client.vue
@@ -187,36 +187,45 @@ export default {
         }
       })
       .on("NOTIFICATION", (msg) => {
-        if (msg.val.kind === "instance") {
-          this.$store.newInstanceValue(
+        if (msg.kind == "composite") {
+          this.$store.newNodes(this.$route.params.endpoint, msg.val);
+          this.$store.setCompositePathsObserved(
             this.$route.params.endpoint,
-            msg.res,
-            msg.val.resources,
-            false
+            msg.paths,
+            true
           );
-        } else if (msg.val.kind === "singleResource") {
-          this.$store.newSingleResourceValue(
-            this.$route.params.endpoint,
-            msg.res,
-            msg.val.value,
-            false
-          );
-        } else if (msg.val.kind === "multiResource") {
-          this.$store.newMultiResourceValue(
-            this.$route.params.endpoint,
-            msg.res,
-            msg.val.values,
-            false
-          );
-        } else if (msg.val.kind === "resourceInstance") {
-          this.$store.newResourceInstanceValueFromPath(
-            this.$route.params.endpoint,
-            msg.res,
-            msg.val.value,
-            false
-          );
+        } else if (msg.kind == "single") {
+          if (msg.val.kind === "instance") {
+            this.$store.newInstanceValue(
+              this.$route.params.endpoint,
+              msg.res,
+              msg.val.resources,
+              false
+            );
+          } else if (msg.val.kind === "singleResource") {
+            this.$store.newSingleResourceValue(
+              this.$route.params.endpoint,
+              msg.res,
+              msg.val.value,
+              false
+            );
+          } else if (msg.val.kind === "multiResource") {
+            this.$store.newMultiResourceValue(
+              this.$route.params.endpoint,
+              msg.res,
+              msg.val.values,
+              false
+            );
+          } else if (msg.val.kind === "resourceInstance") {
+            this.$store.newResourceInstanceValueFromPath(
+              this.$route.params.endpoint,
+              msg.res,
+              msg.val.value,
+              false
+            );
+          }
+          this.$store.setObserved(this.$route.params.endpoint, msg.res, true);
         }
-        this.$store.setObserved(this.$route.params.endpoint, msg.res, true);
       })
       .on("error", (err) => {
         console.error("sse unexpected error", err);

leshan-server-demo/webapp/src/views/CompositeObjectView.vue
@@ -21,6 +21,13 @@
           :endpoint="$route.params.endpoint"
           :compositeObject="compositeObject"
         />
+        <v-icon
+          class="pl-3"
+          small
+          v-show="state.compositeObserved[compositeObservationKey]"
+          :title="compositeObservationKey + ' observed'"
+          >mdi-eye-outline</v-icon
+        >
       </h3>
       <p>
         This Composite object is composed of following resources :
@@ -169,6 +176,9 @@ export default {
     },
     state() {
       return this.$store.state[this.endpoint];
+    },
+    compositeObservationKey(){
+      return this.$store.compositeObjectToKey(this.compositeObject);
     },
     lwm2mNodes() {
       if (!this.objectdefs || this.objectdefs.length == 0) return [];

commit cb84a3d21bd1af77fc3cf86b7dbc06a67221483a

Fix Observe regression in server-demo introduced by c30fe46b1c36e

leshan-server-demo/src/main/java/org/eclipse/leshan/server/demo/servlet/EventServlet.java
@@ -176,7 +176,7 @@ public class EventServlet extends EventSourceServlet {
                 String data = new StringBuilder("{\"ep\":\"") //
                         .append(registration.getEndpoint()) //
                         .append("\",\"kind\":\"single\"") //
-                        .append("\",\"res\":\"") //
+                        .append(",\"res\":\"") //
                         .append(observation.getPath()).append("\",\"val\":") //
                         .append(jsonContent) //
                         .append("}") //