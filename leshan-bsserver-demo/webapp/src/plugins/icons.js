eclipse-leshan/leshan

commit accc6437ea4233fe35b1eb34f4be155576acbf29

Avoid to load whole material design font.
Webpack tree shaking will be used :
https://stackoverflow.com/questions/57552261/vuetifyjs-adding-only-used-icons-to-build

leshan-bsserver-demo/webapp/src/plugins/icons.js
@@ -1,0 +1,77 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Sierra Wireless and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ *
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v20.html
+ * and the Eclipse Distribution License is available at
+ *    http://www.eclipse.org/org/documents/edl-v10.html.
+ *******************************************************************************/
+
+import Vue from "vue";
+import {
+  mdiAlertOutline,
+  mdiCloseCircleOutline,
+  mdiAccountCancelOutline,
+  mdiDatabaseRemove,
+  mdiAccountCheckOutline,
+  mdiCheck,
+  mdiCheckBold,
+  mdiDelete,
+  mdiDeleteOutline,
+  mdiDevices,
+  mdiExclamationThick,
+  mdiInformationOutline,
+  mdiKeyPlus,
+  mdiLeadPencil,
+  mdiPlay,
+  mdiMagnify,
+  mdiServerSecurity,
+} from "@mdi/js";
+
+/**
+ * We create a Icons plugin to load plugin using @mdi/js.
+ * This will allow to use wepback treeshaking, and so only embedded used icons instead of the whole mdi font
+ * See :
+ *   https://stackoverflow.com/questions/57552261/vuetifyjs-adding-only-used-icons-to-build
+ *   https://github.com/vuetifyjs/vuetify/issues/8265
+ */
+
+// create plugin which make data accessible on all vues
+const _icons = {
+  mdiAlertOutline,
+  mdiCloseCircleOutline,
+  mdiAccountCancelOutline,
+  mdiDatabaseRemove,
+  mdiAccountCheckOutline,
+  mdiCheck,
+  mdiCheckBold,
+  mdiDelete,
+  mdiDeleteOutline,
+  mdiDevices,
+  mdiExclamationThick,
+  mdiInformationOutline,
+  mdiKeyPlus,
+  mdiLeadPencil,
+  mdiPlay,
+  mdiMagnify,
+  mdiServerSecurity,
+};
+
+let IconsPlugin = {};
+IconsPlugin.install = function (Vue) {
+  Object.defineProperties(Vue.prototype, {
+    $icons: {
+      get() {
+        return _icons;
+      },
+    },
+  });
+};
+
+Vue.use(IconsPlugin);
+
+export default IconsPlugin;

leshan-server-demo/webapp/src/plugins/icons.js
@@ -1,0 +1,103 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Sierra Wireless and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ *
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v20.html
+ * and the Eclipse Distribution License is available at
+ *    http://www.eclipse.org/org/documents/edl-v10.html.
+ *******************************************************************************/
+
+import Vue from "vue";
+import {
+  mdiAccessPointNetwork,
+  mdiAlertCircle,
+  mdiCheckboxBlankOutline,
+  mdiCertificate,
+  mdiCloseBoxOutline,
+  mdiCogOutline,
+  mdiDelete,
+  mdiDevices,
+  mdiDownload,
+  mdiExitRun,
+  mdiEyeOutline,
+  mdiEyeRemoveOutline,
+  mdiFormatListCheckbox,
+  mdiHelpBox,
+  mdiHelpCircleOutline,
+  mdiHelpRhombusOutline,
+  mdiInformation,
+  mdiInformationOutline,
+  mdiKey,
+  mdiKeyChange,
+  mdiKeyPlus,
+  mdiLock,
+  mdiLockOpenRemove,
+  mdiMagnify,
+  mdiPencil,
+  mdiSelect,
+  mdiServerSecurity,
+  mdiShieldKey,
+  mdiSleep,
+  mdiTune,
+} from "@mdi/js";
+
+/**
+ * We create a Icons plugin to load plugin using @mdi/js.
+ * This will allow to use wepback treeshaking, and so only embedded used icons instead of the whole mdi font
+ * See :
+ *   https://stackoverflow.com/questions/57552261/vuetifyjs-adding-only-used-icons-to-build
+ *   https://github.com/vuetifyjs/vuetify/issues/8265
+ */
+
+// create plugin which make data accessible on all vues
+const _icons = {
+  mdiAccessPointNetwork,
+  mdiAlertCircle,
+  mdiCheckboxBlankOutline,
+  mdiCertificate,
+  mdiCloseBoxOutline,
+  mdiCogOutline,
+  mdiDelete,
+  mdiDevices,
+  mdiDownload,
+  mdiExitRun,
+  mdiEyeOutline,
+  mdiEyeRemoveOutline,
+  mdiFormatListCheckbox,
+  mdiHelpBox,
+  mdiHelpCircleOutline,
+  mdiHelpRhombusOutline,
+  mdiInformation,
+  mdiInformationOutline,
+  mdiKey,
+  mdiKeyChange,
+  mdiKeyPlus,
+  mdiLock,
+  mdiLockOpenRemove,
+  mdiMagnify,
+  mdiPencil,
+  mdiSelect,
+  mdiServerSecurity,
+  mdiShieldKey,
+  mdiSleep,
+  mdiTune,
+};
+
+let IconsPlugin = {};
+IconsPlugin.install = function (Vue) {
+  Object.defineProperties(Vue.prototype, {
+    $icons: {
+      get() {
+        return _icons;
+      },
+    },
+  });
+};
+
+Vue.use(IconsPlugin);
+
+export default IconsPlugin;

leshan-bsserver-demo/webapp/src/components/LeshanNavBar.vue
@@ -41,12 +41,24 @@
 
 <script>
 export default {
-  data: function() {
+  data: function () {
     return {
       pages: [
-        { title: "Bootstrap", route: "/bootstrap", icon: "mdi-devices" },
-        { title: "Server", route: "/server", icon: "mdi-server-security" },
-        { title: "About", route: "/about", icon: "mdi-information-outline" },
+        {
+          title: "Bootstrap",
+          route: "/bootstrap",
+          icon: this.$icons.mdiDevices,
+        },
+        {
+          title: "Server",
+          route: "/server",
+          icon: this.$icons.mdiServerSecurity,
+        },
+        {
+          title: "About",
+          route: "/about",
+          icon: this.$icons.mdiInformationOutline,
+        },
       ],
     };
   },

leshan-bsserver-demo/webapp/src/main.js
@@ -13,6 +13,7 @@
 
 import Vue from 'vue'
 import './plugins/axios'
+import './plugins/icons'
 import './plugins/dialog'
 import './plugins/moment'
 import './plugins/sse'

leshan-bsserver-demo/webapp/src/plugins/vuetify.js
@@ -1,18 +1,17 @@
 /*******************************************************************************
  * Copyright (c) 2021 Sierra Wireless and others.
- * 
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * and Eclipse Distribution License v1.0 which accompany this distribution.
- * 
+ *
  * The Eclipse Public License is available at
  *    http://www.eclipse.org/legal/epl-v20.html
  * and the Eclipse Distribution License is available at
  *    http://www.eclipse.org/org/documents/edl-v10.html.
  *******************************************************************************/
 
-import '@mdi/font/css/materialdesignicons.css';
-import "@fontsource/roboto"
+import "@fontsource/roboto";
 import Vue from "vue";
 import Vuetify from "vuetify/lib/framework";
 
@@ -20,6 +19,6 @@ Vue.use(Vuetify);
 
 export default new Vuetify({
   icons: {
-    iconfont: "mdi", // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
+    iconfont: "mdiSvg", // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
   },
 });

leshan-bsserver-demo/webapp/src/views/Bootstrap.vue
@@ -35,7 +35,7 @@
           ></v-divider>
           <v-text-field
             v-model="search"
-            append-icon="mdi-magnify"
+            :append-icon="$icons.mdiMagnify"
             label="Search"
             single-line
             hide-details
@@ -49,7 +49,7 @@
               $vuetify.breakpoint.smAndDown ? "" : "Add Clients Configuration"
             }}
             <v-icon :right="!$vuetify.breakpoint.smAndDown" dark>
-              mdi-key-plus
+              {{ $icons.mdiKeyPlus }}
             </v-icon>
           </v-btn>
 
@@ -107,7 +107,9 @@
       </template>
       <!--custom display for "actions" column-->
       <template v-slot:item.actions="{ item }">
-        <v-icon small @click.stop="onDelete(item)"> mdi-delete </v-icon>
+        <v-icon small @click.stop="onDelete(item)">
+          {{ $icons.mdiDelete }}
+        </v-icon>
       </template>
     </v-data-table>
   </div>

leshan-bsserver-demo/webapp/src/views/Client.vue
@@ -11,15 +11,17 @@
  *    http://www.eclipse.org/org/documents/edl-v10.html.
   ----------------------------------------------------------------------------->
 <template>
-  <v-card elevation="0" >
+  <v-card elevation="0">
     <v-card-text>
-      <v-card-title class="justify-center"> Client "{{ $route.params.endpoint }}" </v-card-title>
+      <v-card-title class="justify-center">
+        Client "{{ $route.params.endpoint }}"
+      </v-card-title>
 
-      <v-card-subtitle class="text-center" >
+      <v-card-subtitle class="text-center">
         Watch your client bootstrap session.
 
         <v-btn icon title="Clear timeline" @click="events = []">
-          <v-icon>mdi-delete-sweep-outline</v-icon>
+          <v-icon>{{ $icons.mdiDeleteSweepOutline }}</v-icon>
         </v-btn>
       </v-card-subtitle>
 
@@ -75,7 +77,7 @@ export default {
         event.icon = this.getIcon(event);
         event.color = this.getColor(event);
         this.events.push(event);
-        this.$nextTick(function() {
+        this.$nextTick(function () {
           this.$vuetify.goTo(this.$refs.end);
         });
       })
@@ -96,18 +98,18 @@ export default {
 
     getIcon(event) {
       let icons = {
-        "new session": "mdi-play",
-        unauthorized: "mdi-account-cancel-outline",
-        "no config": "mdi-database-remove",
-        authorized: "mdi-account-check-outline",
-        "send write": "mdi-lead-pencil",
-        "send delete": "mdi-delete-outline",
-        "send discover": "mdi-magnify",
-        "receive success response": "mdi-check",
-        "receive error response": "mdi-alert-outline",
-        finished: "mdi-check-bold",
-        "request failure": "mdi-close-circle-outline",
-        failed: "mdi-exclamation-thick",
+        "new session": this.$icons.mdiPlay,
+        unauthorized: this.$icons.mdiAccountCancelOutline,
+        "no config": this.$icons.mdiDatabaseRemove,
+        authorized: this.$icons.mdiAccountCheckOutline,
+        "send write": this.$icons.mdiLeadPencil,
+        "send delete": this.$icons.mdiDeleteOutline,
+        "send discover": this.$icons.mdiMagnify,
+        "receive success response": this.$icons.mdiCheck,
+        "receive error response": this.$icons.mdiAlertOutline,
+        finished: this.$icons.mdiCheckBold,
+        "request failure": this.$icons.mdiCloseCircleOutline,
+        failed: this.$icons.mdiExclamationThick,
       };
       return icons[event.name];
     },

leshan-server-core-demo/webapp/src/components/path/PathsInput.vue
@@ -30,7 +30,7 @@
         required
         dense
         @input="updatePath(index, $event)"
-        append-outer-icon="mdi-delete"
+        :append-outer-icon="$icon.mdiDelete"
         @click:append-outer="removePath(index)"
       ></v-text-field>
     </div>

commit 31d2dc9ecc7f9cb7c4e64d74e1904f8137e1a90d

Fix some missing icons in bsserver demo (regression from accc6437)

leshan-bsserver-demo/webapp/src/plugins/icons.js
@@ -13,22 +13,27 @@
 
 import Vue from "vue";
 import {
-  mdiAlertOutline,
-  mdiCloseCircleOutline,
+  mdiAccessPointNetwork,
   mdiAccountCancelOutline,
-  mdiDatabaseRemove,
   mdiAccountCheckOutline,
+  mdiAlertOutline,
+  mdiCertificate,
   mdiCheck,
   mdiCheckBold,
+  mdiCloseCircleOutline,
+  mdiDatabaseRemove,
   mdiDelete,
   mdiDeleteOutline,
   mdiDevices,
+  mdiDownload,
   mdiExclamationThick,
   mdiInformationOutline,
+  mdiKey,
   mdiKeyPlus,
   mdiLeadPencil,
-  mdiPlay,
+  mdiLockOpenRemove,
   mdiMagnify,
+  mdiPlay,
   mdiServerSecurity,
 } from "@mdi/js";
 
@@ -42,22 +47,27 @@ import {
 
 // create plugin which make data accessible on all vues
 const _icons = {
-  mdiAlertOutline,
-  mdiCloseCircleOutline,
+  mdiAccessPointNetwork,
   mdiAccountCancelOutline,
-  mdiDatabaseRemove,
   mdiAccountCheckOutline,
+  mdiAlertOutline,
+  mdiCertificate,
   mdiCheck,
   mdiCheckBold,
+  mdiCloseCircleOutline,
+  mdiDatabaseRemove,
   mdiDelete,
   mdiDeleteOutline,
   mdiDevices,
+  mdiDownload,
   mdiExclamationThick,
   mdiInformationOutline,
+  mdiKey,
   mdiKeyPlus,
   mdiLeadPencil,
-  mdiPlay,
+  mdiLockOpenRemove,
   mdiMagnify,
+  mdiPlay,
   mdiServerSecurity,
 };
 