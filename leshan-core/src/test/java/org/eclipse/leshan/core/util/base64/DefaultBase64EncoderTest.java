9a8319e8dd42d98ba5d4efc64f5c643366d8f9b0
Warmek <bartosz.stolarczyk@orange.com>
2022-12-06 10:04:55 +0100

    Added headers with contributors

--- leshan-core/src/test/java/org/eclipse/leshan/core/util/base64/DefaultBase64EncoderTest.java
+++ leshan-core/src/test/java/org/eclipse/leshan/core/util/base64/DefaultBase64EncoderTest.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ *
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v20.html
+ * and the Eclipse Distribution License is available at
+ *    http://www.eclipse.org/org/documents/edl-v10.html.
+ *
+ * Contributors:
+ *     Bartosz Stolarczyk
+ *     Orange Polska S.A. - initial API and implementation
+ *******************************************************************************/
+
 package org.eclipse.leshan.core.util.base64;
 
 import static org.junit.Assert.assertEquals;
