e88d8f2065d1902399e0b1b6745fe6e5a22dcafc
Simon Bernard <sbernard@sierrawireless.com>
2023-01-27 11:47:58 +0100

    Convert leshan-core-cf tests to pure Junit5

--- leshan-core-cf/src/test/java/org/eclipse/leshan/core/californium/ResponseCodeUtilTest.java
+++ leshan-core-cf/src/test/java/org/eclipse/leshan/core/californium/ResponseCodeUtilTest.java
@@ -15,9 +15,11 @@
  *******************************************************************************/
 package org.eclipse.leshan.core.californium;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+
 import org.eclipse.californium.core.coap.CoAP.ResponseCode;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class ResponseCodeUtilTest {
 
@@ -26,8 +28,8 @@ public class ResponseCodeUtilTest {
         org.eclipse.leshan.core.ResponseCode lwM2mResponseCode = ResponseCodeUtil
                 .toLwM2mResponseCode(ResponseCode.CREATED);
 
-        Assert.assertEquals(org.eclipse.leshan.core.ResponseCode.CREATED, lwM2mResponseCode);
-        Assert.assertEquals("CREATED(201)", lwM2mResponseCode.toString());
+        assertEquals(org.eclipse.leshan.core.ResponseCode.CREATED, lwM2mResponseCode);
+        assertEquals("CREATED(201)", lwM2mResponseCode.toString());
     }
 
     @Test
@@ -35,9 +37,9 @@ public class ResponseCodeUtilTest {
         org.eclipse.leshan.core.ResponseCode lwM2mResponseCode = ResponseCodeUtil
                 .toLwM2mResponseCode(ResponseCode.GATEWAY_TIMEOUT);
 
-        Assert.assertEquals(504, lwM2mResponseCode.getCode());
-        Assert.assertEquals(org.eclipse.leshan.core.ResponseCode.UNKNOWN, lwM2mResponseCode.getName());
-        Assert.assertEquals("UNKNOWN(504)", lwM2mResponseCode.toString());
+        assertEquals(504, lwM2mResponseCode.getCode());
+        assertEquals(org.eclipse.leshan.core.ResponseCode.UNKNOWN, lwM2mResponseCode.getName());
+        assertEquals("UNKNOWN(504)", lwM2mResponseCode.toString());
     }
 
     @Test
@@ -45,7 +47,7 @@ public class ResponseCodeUtilTest {
         ResponseCode coapResponseCode = ResponseCodeUtil
                 .toCoapResponseCode(org.eclipse.leshan.core.ResponseCode.BAD_REQUEST);
 
-        Assert.assertEquals(ResponseCode.BAD_REQUEST, coapResponseCode);
+        assertEquals(ResponseCode.BAD_REQUEST, coapResponseCode);
     }
 
     @Test
@@ -53,17 +55,21 @@ public class ResponseCodeUtilTest {
         ResponseCode coapResponseCode = ResponseCodeUtil
                 .toCoapResponseCode(new org.eclipse.leshan.core.ResponseCode(503));
 
-        Assert.assertEquals(ResponseCode.SERVICE_UNAVAILABLE, coapResponseCode);
+        assertEquals(ResponseCode.SERVICE_UNAVAILABLE, coapResponseCode);
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test()
     public void unknown_lwm2m_code_to_invalid_coap_code() {
-        ResponseCodeUtil.toCoapResponseCode(new org.eclipse.leshan.core.ResponseCode(301));
+        assertThrowsExactly(IllegalArgumentException.class, () -> {
+            ResponseCodeUtil.toCoapResponseCode(new org.eclipse.leshan.core.ResponseCode(301));
+        });
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test()
     public void unknown_lwm2m_code_to_invalid_coap_code2() {
-        ResponseCodeUtil.toCoapResponseCode(new org.eclipse.leshan.core.ResponseCode(441));
+        assertThrowsExactly(IllegalArgumentException.class, () -> {
+            ResponseCodeUtil.toCoapResponseCode(new org.eclipse.leshan.core.ResponseCode(441));
+        });
     }
 
     @Test
@@ -73,15 +79,15 @@ public class ResponseCodeUtilTest {
         // for success : code value is lost but we know we use an unknown code
         ResponseCode coapResponseCode = ResponseCodeUtil
                 .toCoapResponseCode(new org.eclipse.leshan.core.ResponseCode(206));
-        Assert.assertEquals(ResponseCode._UNKNOWN_SUCCESS_CODE, coapResponseCode);
+        assertEquals(ResponseCode._UNKNOWN_SUCCESS_CODE, coapResponseCode);
 
         // for client error,: unknown code is replace by BAD REQUEST ...
         coapResponseCode = ResponseCodeUtil.toCoapResponseCode(new org.eclipse.leshan.core.ResponseCode(425));
-        Assert.assertEquals(ResponseCode.BAD_REQUEST, coapResponseCode);
+        assertEquals(ResponseCode.BAD_REQUEST, coapResponseCode);
 
         // for server error : unknown code is replace by INTERNAL SERVER ERROR ...
         coapResponseCode = ResponseCodeUtil.toCoapResponseCode(new org.eclipse.leshan.core.ResponseCode(509));
-        Assert.assertEquals(ResponseCode.INTERNAL_SERVER_ERROR, coapResponseCode);
+        assertEquals(ResponseCode.INTERNAL_SERVER_ERROR, coapResponseCode);
 
     }
 }
