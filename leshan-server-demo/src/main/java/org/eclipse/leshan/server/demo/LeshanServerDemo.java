9be2094b6c740e37020c33e76eb7c4a6ddba8c61
Simon Bernard <sbernard@sierrawireless.com>
2023-02-17 10:10:29 +0100

    avoid variable not used

--- leshan-server-demo/src/main/java/org/eclipse/leshan/server/demo/LeshanServerDemo.java
+++ leshan-server-demo/src/main/java/org/eclipse/leshan/server/demo/LeshanServerDemo.java
@@ -17,17 +17,23 @@
  *******************************************************************************/
 package org.eclipse.leshan.server.demo;
 
+import java.io.File;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.URI;
 import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
 import java.util.List;
 
 import javax.jmdns.JmDNS;
 import javax.jmdns.ServiceInfo;
 
+import org.eclipse.californium.core.config.CoapConfig;
 import org.eclipse.californium.elements.config.Configuration;
+import org.eclipse.californium.elements.util.CertPathUtil;
+import org.eclipse.californium.scandium.config.DtlsConfig;
+import org.eclipse.californium.scandium.config.DtlsConfig.DtlsRole;
 import org.eclipse.californium.scandium.config.DtlsConnectorConfig.Builder;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletHolder;
@@ -35,12 +41,17 @@ import org.eclipse.jetty.webapp.WebAppContext;
 import org.eclipse.leshan.core.californium.PrincipalMdcConnectionListener;
 import org.eclipse.leshan.core.demo.LwM2mDemoConstant;
 import org.eclipse.leshan.core.demo.cli.ShortErrorMessageHandler;
+import org.eclipse.leshan.core.endpoint.EndpointUriUtil;
+import org.eclipse.leshan.core.endpoint.Protocol;
 import org.eclipse.leshan.core.model.ObjectLoader;
 import org.eclipse.leshan.core.model.ObjectModel;
 import org.eclipse.leshan.server.LeshanServer;
 import org.eclipse.leshan.server.LeshanServerBuilder;
 import org.eclipse.leshan.server.californium.endpoint.CaliforniumServerEndpointFactory;
+import org.eclipse.leshan.server.californium.endpoint.CaliforniumServerEndpointsProvider;
 import org.eclipse.leshan.server.californium.endpoint.ServerProtocolProvider;
+import org.eclipse.leshan.server.californium.endpoint.coap.CoapOscoreServerEndpointFactory;
+import org.eclipse.leshan.server.californium.endpoint.coap.CoapServerProtocolProvider;
 import org.eclipse.leshan.server.californium.endpoint.coaps.CoapsServerEndpointFactory;
 import org.eclipse.leshan.server.californium.endpoint.coaps.CoapsServerProtocolProvider;
 import org.eclipse.leshan.server.core.demo.json.servlet.SecurityServlet;
@@ -194,66 +205,66 @@ public class LeshanServerDemo {
         };
 
         // Create Server Endpoints Provider
-//        CaliforniumServerEndpointsProvider.Builder endpointsBuilder = new CaliforniumServerEndpointsProvider.Builder(
-//                new CoapServerProtocolProvider(), coapsProtocolProvider);
-//
-//        // Create Californium Configuration
-//        Configuration serverCoapConfig = endpointsBuilder.createDefaultConfiguration();
-//
-//        // Set some DTLS stuff
-//        // These configuration values are always overwritten by CLI therefore set them to transient.
-//        serverCoapConfig.setTransient(DtlsConfig.DTLS_RECOMMENDED_CIPHER_SUITES_ONLY);
-//        serverCoapConfig.set(DtlsConfig.DTLS_RECOMMENDED_CIPHER_SUITES_ONLY, !cli.dtls.supportDeprecatedCiphers);
-//        serverCoapConfig.setTransient(DtlsConfig.DTLS_CONNECTION_ID_LENGTH);
-//        if (cli.dtls.cid != null) {
-//            serverCoapConfig.set(DtlsConfig.DTLS_CONNECTION_ID_LENGTH, cli.dtls.cid);
-//        }
-//
-//        // Persist configuration
-//        File configFile = new File(CF_CONFIGURATION_FILENAME);
-//        if (configFile.isFile()) {
-//            serverCoapConfig.load(configFile);
-//        } else {
-//            serverCoapConfig.store(configFile, CF_CONFIGURATION_HEADER);
-//        }
-//
-//        // Enforce DTLS role to ServerOnly if needed
-//        if (cli.identity.isx509()) {
-//            X509Certificate serverCertificate = cli.identity.getCertChain()[0];
-//            if (serverCoapConfig.get(DtlsConfig.DTLS_ROLE) == DtlsRole.BOTH) {
-//                if (serverCertificate != null) {
-//                    if (CertPathUtil.canBeUsedForAuthentication(serverCertificate, false)) {
-//                        if (!CertPathUtil.canBeUsedForAuthentication(serverCertificate, true)) {
-//                            serverCoapConfig.set(DtlsConfig.DTLS_ROLE, DtlsRole.SERVER_ONLY);
-//                            LOG.warn("Server certificate does not allow Client Authentication usage."
-//                                    + "\nThis will prevent this LWM2M server to initiate DTLS connection."
-//                                    + "\nSee : https://github.com/eclipse/leshan/wiki/Server-Failover#about-connections");
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//
-//        // Set Californium Configuration
-//        endpointsBuilder.setConfiguration(serverCoapConfig);
-//
-//        // Create CoAP endpoint
-//        int coapPort = cli.main.localPort == null ? serverCoapConfig.get(CoapConfig.COAP_PORT) : cli.main.localPort;
-//        InetSocketAddress coapAddr = cli.main.localAddress == null ? new InetSocketAddress(coapPort)
-//                : new InetSocketAddress(cli.main.localAddress, coapPort);
-//        if (cli.main.disableOscore) {
-//            endpointsBuilder.addEndpoint(coapAddr, Protocol.COAP);
-//        } else {
-//            endpointsBuilder.addEndpoint(new CoapOscoreServerEndpointFactory(
-//                    EndpointUriUtil.createUri(Protocol.COAP.getUriScheme(), coapAddr)));
-//        }
-//
-//        // Create CoAP over DTLS endpoint
-//        int coapsPort = cli.main.secureLocalPort == null ? serverCoapConfig.get(CoapConfig.COAP_SECURE_PORT)
-//                : cli.main.secureLocalPort;
-//        InetSocketAddress coapsAddr = cli.main.secureLocalAddress == null ? new InetSocketAddress(coapsPort)
-//                : new InetSocketAddress(cli.main.secureLocalAddress, coapsPort);
-//        endpointsBuilder.addEndpoint(coapsAddr, Protocol.COAPS);
+        CaliforniumServerEndpointsProvider.Builder endpointsBuilder = new CaliforniumServerEndpointsProvider.Builder(
+                new CoapServerProtocolProvider(), coapsProtocolProvider);
+
+        // Create Californium Configuration
+        Configuration serverCoapConfig = endpointsBuilder.createDefaultConfiguration();
+
+        // Set some DTLS stuff
+        // These configuration values are always overwritten by CLI therefore set them to transient.
+        serverCoapConfig.setTransient(DtlsConfig.DTLS_RECOMMENDED_CIPHER_SUITES_ONLY);
+        serverCoapConfig.set(DtlsConfig.DTLS_RECOMMENDED_CIPHER_SUITES_ONLY, !cli.dtls.supportDeprecatedCiphers);
+        serverCoapConfig.setTransient(DtlsConfig.DTLS_CONNECTION_ID_LENGTH);
+        if (cli.dtls.cid != null) {
+            serverCoapConfig.set(DtlsConfig.DTLS_CONNECTION_ID_LENGTH, cli.dtls.cid);
+        }
+
+        // Persist configuration
+        File configFile = new File(CF_CONFIGURATION_FILENAME);
+        if (configFile.isFile()) {
+            serverCoapConfig.load(configFile);
+        } else {
+            serverCoapConfig.store(configFile, CF_CONFIGURATION_HEADER);
+        }
+
+        // Enforce DTLS role to ServerOnly if needed
+        if (cli.identity.isx509()) {
+            X509Certificate serverCertificate = cli.identity.getCertChain()[0];
+            if (serverCoapConfig.get(DtlsConfig.DTLS_ROLE) == DtlsRole.BOTH) {
+                if (serverCertificate != null) {
+                    if (CertPathUtil.canBeUsedForAuthentication(serverCertificate, false)) {
+                        if (!CertPathUtil.canBeUsedForAuthentication(serverCertificate, true)) {
+                            serverCoapConfig.set(DtlsConfig.DTLS_ROLE, DtlsRole.SERVER_ONLY);
+                            LOG.warn("Server certificate does not allow Client Authentication usage."
+                                    + "\nThis will prevent this LWM2M server to initiate DTLS connection."
+                                    + "\nSee : https://github.com/eclipse/leshan/wiki/Server-Failover#about-connections");
+                        }
+                    }
+                }
+            }
+        }
+
+        // Set Californium Configuration
+        endpointsBuilder.setConfiguration(serverCoapConfig);
+
+        // Create CoAP endpoint
+        int coapPort = cli.main.localPort == null ? serverCoapConfig.get(CoapConfig.COAP_PORT) : cli.main.localPort;
+        InetSocketAddress coapAddr = cli.main.localAddress == null ? new InetSocketAddress(coapPort)
+                : new InetSocketAddress(cli.main.localAddress, coapPort);
+        if (cli.main.disableOscore) {
+            endpointsBuilder.addEndpoint(coapAddr, Protocol.COAP);
+        } else {
+            endpointsBuilder.addEndpoint(new CoapOscoreServerEndpointFactory(
+                    EndpointUriUtil.createUri(Protocol.COAP.getUriScheme(), coapAddr)));
+        }
+
+        // Create CoAP over DTLS endpoint
+        int coapsPort = cli.main.secureLocalPort == null ? serverCoapConfig.get(CoapConfig.COAP_SECURE_PORT)
+                : cli.main.secureLocalPort;
+        InetSocketAddress coapsAddr = cli.main.secureLocalAddress == null ? new InetSocketAddress(coapsPort)
+                : new InetSocketAddress(cli.main.secureLocalAddress, coapsPort);
+        endpointsBuilder.addEndpoint(coapsAddr, Protocol.COAPS);
 
         // Create LWM2M server
         // builder.setEndpointsProvider(endpointsBuilder.build());
