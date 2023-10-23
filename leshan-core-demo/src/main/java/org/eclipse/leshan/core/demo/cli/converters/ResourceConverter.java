b14e4932db0f043c34c803986d3487463924c027
mgdlkundera <magdalena.kundera@orange.com>
2023-10-23 10:30:49 +0200

    Correction in ResourceConverter

--- leshan-core-demo/src/main/java/org/eclipse/leshan/core/demo/cli/converters/ResourceConverter.java
+++ leshan-core-demo/src/main/java/org/eclipse/leshan/core/demo/cli/converters/ResourceConverter.java
@@ -14,36 +14,16 @@
  *******************************************************************************/
 package org.eclipse.leshan.core.demo.cli.converters;
 
-import org.eclipse.leshan.core.node.InvalidLwM2mPathException;
-import org.eclipse.leshan.core.node.LwM2mNodeUtil;
+import org.eclipse.leshan.core.node.LwM2mNodeException;
 import org.eclipse.leshan.core.node.LwM2mPath;
-import org.eclipse.leshan.core.util.Validate;
 
-import picocli.CommandLine.ITypeConverter;
-
-public class ResourceConverter implements ITypeConverter<LwM2mPath> {
+public class ResourceConverter extends LwM2mPathConverter {
 
     @Override
-    public LwM2mPath convert(String path) throws Exception {
-        Validate.notNull(path);
-        if (path.startsWith("/")) {
-            path = path.substring(1);
-        }
-        if (path.endsWith("/")) {
-            path = path.substring(0, path.length() - 1);
-        }
-        String[] p = path.split("/");
-        if (0 > p.length || p.length > 4)
-            throw new InvalidLwM2mPathException("Invalid length for path: ", path);
-
-        Integer resourceId = (p.length >= 3) ? Integer.valueOf(p[2]) : null;
-        Integer resourceInstanceId = (p.length == 4) ? Integer.valueOf(p[3]) : null;
-
-        if (resourceId != null)
-            LwM2mNodeUtil.validateResourceId(resourceId);
-        if (resourceInstanceId != null)
-            LwM2mNodeUtil.validateResourceInstanceId(resourceInstanceId);
-
-        return new LwM2mPath(path);
+    public LwM2mPath convert(String path) {
+        LwM2mPath lwM2mPath = new LwM2mPath(path);
+        if (!lwM2mPath.isResource())
+            throw new LwM2mNodeException("Created path is not a path identifying a resource");
+        return lwM2mPath;
     }
 }
