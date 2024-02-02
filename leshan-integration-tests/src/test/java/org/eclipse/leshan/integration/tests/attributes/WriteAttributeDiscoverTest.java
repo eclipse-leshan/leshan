b585b10c4dba35f78524e1feecf6db3180cd59f9
Simon Bernard <code@simonbernard.eu>
2024-02-02 17:21:25 +0100

    First support of write attribute for java-coap

--- leshan-integration-tests/src/test/java/org/eclipse/leshan/integration/tests/attributes/WriteAttributeDiscoverTest.java
+++ leshan-integration-tests/src/test/java/org/eclipse/leshan/integration/tests/attributes/WriteAttributeDiscoverTest.java
@@ -67,11 +67,10 @@ public class WriteAttributeDiscoverTest {
     static Stream<org.junit.jupiter.params.provider.Arguments> transports() {
         return Stream.of(//
                 // ProtocolUsed - Client Endpoint Provider - Server Endpoint Provider
-                arguments(Protocol.COAP, "Californium", "Californium") // , //
-        // arguments(Protocol.COAP, "Californium", "java-coap"), //
-        // arguments(Protocol.COAP, "java-coap", "Californium"), //
-        // arguments(Protocol.COAP, "java-coap", "java-coap"))
-        );
+                arguments(Protocol.COAP, "Californium", "Californium"), //
+                arguments(Protocol.COAP, "Californium", "java-coap"), //
+                arguments(Protocol.COAP, "java-coap", "Californium"), //
+                arguments(Protocol.COAP, "java-coap", "java-coap"));
     }
 
     /*---------------------------------/
