1a8785920b72f177e0b578f82b94c98fb85657c1
Simon Bernard <code@simonbernard.eu>
2024-02-07 18:07:40 +0100

    Add support of unsigned integer to gt, lt and st

--- leshan-client-core/src/main/java/org/eclipse/leshan/client/notification/checker/UnsignedIntegerChecker.java
+++ leshan-client-core/src/main/java/org/eclipse/leshan/client/notification/checker/UnsignedIntegerChecker.java
@@ -15,16 +15,38 @@
  *******************************************************************************/
 package org.eclipse.leshan.client.notification.checker;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
+
 import org.eclipse.leshan.core.link.lwm2m.attributes.LwM2mAttributeSet;
+import org.eclipse.leshan.core.link.lwm2m.attributes.LwM2mAttributes;
+import org.eclipse.leshan.core.util.datatype.ULong;
 
 public class UnsignedIntegerChecker implements CriteriaBasedOnValueChecker {
 
     @Override
     public boolean shouldTriggerNotificationBasedOnValueChange(LwM2mAttributeSet attributes, Object lastSentValue,
             Object newValue) {
-//        ULong lastSentULong = (ULong) lastSentValue;
-//        ULong newULong = (ULong) newValue;
+        BigInteger lastSentULong = ((ULong) lastSentValue).toBigInteger();
+        BigInteger newULong = ((ULong) newValue).toBigInteger();
 
-        throw new IllegalStateException("Not implemented yet");
+        if (attributes.contains(LwM2mAttributes.STEP)) {
+            // Handle Step
+            BigInteger step = new BigDecimal(attributes.get(LwM2mAttributes.STEP).getValue())
+                    .setScale(0, RoundingMode.CEILING).toBigIntegerExact();
+            return lastSentULong.subtract(newULong).abs().subtract(step).signum() >= 0;
+        } else if (attributes.contains(LwM2mAttributes.LESSER_THAN)) {
+            // Handle LESSER_THAN
+            BigDecimal lessThan = new BigDecimal(attributes.get(LwM2mAttributes.LESSER_THAN).getValue());
+            BigInteger lessThanRounded = lessThan.setScale(0, RoundingMode.CEILING).toBigIntegerExact();
+            return lastSentULong.compareTo(lessThanRounded) >= 0 && newULong.compareTo(lessThanRounded) < 0;
+        } else if (attributes.contains(LwM2mAttributes.GREATER_THAN)) {
+            // Handle LESSER_THAN
+            BigDecimal lessThan = new BigDecimal(attributes.get(LwM2mAttributes.GREATER_THAN).getValue());
+            BigInteger lessThanRounded = lessThan.setScale(0, RoundingMode.FLOOR).toBigIntegerExact();
+            return lastSentULong.compareTo(lessThanRounded) <= 0 && newULong.compareTo(lessThanRounded) > 0;
+        }
+        return true;
     }
 }
