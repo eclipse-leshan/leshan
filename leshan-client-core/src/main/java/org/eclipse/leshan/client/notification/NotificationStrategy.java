031ff46d9fa39dfd4c6fde06348d7899f922fb18
Simon Bernard <code@simonbernard.eu>
2024-02-20 17:33:54 +0100

    Make possible to provide its own Notification Strategy + javadoc

--- leshan-client-core/src/main/java/org/eclipse/leshan/client/notification/NotificationStrategy.java
+++ leshan-client-core/src/main/java/org/eclipse/leshan/client/notification/NotificationStrategy.java
@@ -15,120 +15,66 @@
  *******************************************************************************/
 package org.eclipse.leshan.client.notification;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.eclipse.leshan.client.notification.checker.CriteriaBasedOnValueChecker;
-import org.eclipse.leshan.client.notification.checker.FloatChecker;
-import org.eclipse.leshan.client.notification.checker.IntegerChecker;
-import org.eclipse.leshan.client.notification.checker.UnsignedIntegerChecker;
-import org.eclipse.leshan.core.link.lwm2m.attributes.LwM2mAttribute;
-import org.eclipse.leshan.core.link.lwm2m.attributes.LwM2mAttributeSet;
-import org.eclipse.leshan.core.link.lwm2m.attributes.LwM2mAttributes;
+import org.eclipse.leshan.core.link.lwm2m.attributes.LwM2mAttributeModel;
 import org.eclipse.leshan.core.link.lwm2m.attributes.NotificationAttributeTree;
-import org.eclipse.leshan.core.model.ResourceModel.Type;
 import org.eclipse.leshan.core.node.LwM2mChildNode;
 import org.eclipse.leshan.core.node.LwM2mNode;
 import org.eclipse.leshan.core.node.LwM2mPath;
-import org.eclipse.leshan.core.node.LwM2mResourceInstance;
-import org.eclipse.leshan.core.node.LwM2mSingleResource;
 
-// TODO write attributes : create an interface
-// The way write attribute should be handle is not clear in LWM2M specification (-_-!) ... which is not an ideal situation ...
-// See : https://github.com/OpenMobileAlliance/OMA_LwM2M_for_Developers/issues/478
-// So we create a dedicated class about how this attributes should be handled.
-// The idea will be to create a Interface + Default implement and let users implements their own strategy depending of their understanding or needs.
-public class NotificationStrategy {
+/**
+ * This class is responsible to implement how write attribute are assigned to given LWM2M node and how it should be
+ * handle.
+ * <p>
+ * This aims to provide some flexibility because LWM2M specification isn't really clear on this topic.
+ *
+ * @see <a href="https://github.com/OpenMobileAlliance/OMA_LwM2M_for_Developers/issues/478"> Discussion about Write
+ *      Attributes on OMA LwM2M for Developers Github repository </a>
+ * @see DefaultNotificationStrategy
+ */
+public interface NotificationStrategy {
 
-    protected Map<Type, CriteriaBasedOnValueChecker> checkers = new HashMap<>();
+    /**
+     * Create a {@link NotificationAttributeTree} when observe relation is initiated with information needed later.
+     *
+     * @param path The path of the LWM2M node to observe
+     * @param attributes The whole {@link NotificationAttributeTree} currently attached to the object targeted by the
+     *        given path.
+     */
+    NotificationAttributeTree selectNotificationsAttributes(LwM2mPath path, NotificationAttributeTree attributes);
 
-    public NotificationStrategy() {
-        checkers.put(Type.FLOAT, new FloatChecker());
-        checkers.put(Type.INTEGER, new IntegerChecker());
-        checkers.put(Type.UNSIGNED_INTEGER, new UnsignedIntegerChecker());
-    }
+    /**
+     * @param attributes The attributes returned by
+     *        {@link #selectNotificationsAttributes(LwM2mPath, NotificationAttributeTree)}
+     * @param path The path of the LWM2M node to observe
+     * @param model The {@link LwM2mAttributeModel} for which we want to know if the given attribute is assigned
+     */
+    boolean hasAttribute(NotificationAttributeTree attributes, LwM2mPath path, LwM2mAttributeModel<?> model);
 
-    public NotificationAttributeTree selectNotificationsAttributes(LwM2mPath path,
-            NotificationAttributeTree attributes) {
+    /**
+     * @param attributes The attributes returned by
+     *        {@link #selectNotificationsAttributes(LwM2mPath, NotificationAttributeTree)}
+     * @param path The path of the LWM2M node to observe
+     * @param model he {@link LwM2mAttributeModel} for which we want to get the value
+     */
+    <T> T getAttributeValue(NotificationAttributeTree attributes, LwM2mPath path, LwM2mAttributeModel<T> model);
 
-        LwM2mAttributeSet set = attributes.getWithInheritance(path);
-        if (set == null || set.isEmpty()) {
-            return null;
-        }
-        NotificationAttributeTree result = new NotificationAttributeTree();
-        result.put(path, set);
-        return result;
-    }
+    /**
+     * @param attributes The attributes returned by
+     *        {@link #selectNotificationsAttributes(LwM2mPath, NotificationAttributeTree)}
+     * @param path The path of the LWM2M node to observe
+     * @return <code>True</code> some attributes assigned are based on value (e.g. gt, lt, st)
+     */
+    boolean hasCriteriaBasedOnValue(NotificationAttributeTree attributes, LwM2mPath path);
 
-    public boolean hasPmin(NotificationAttributeTree attributes, LwM2mPath path) {
-        return getPmin(path, attributes) != null;
-    }
-
-    public Long getPmin(LwM2mPath path, NotificationAttributeTree attributes) {
-        LwM2mAttributeSet set = attributes.get(path);
-        if (set == null)
-            return null;
-        else {
-            LwM2mAttribute<Long> pmin = set.get(LwM2mAttributes.MINIMUM_PERIOD);
-            if (pmin == null)
-                return null;
-            else
-                return pmin.getValue();
-        }
-    }
-
-    public boolean hasPmax(LwM2mPath path, NotificationAttributeTree attributes) {
-        return getPmax(path, attributes) != null;
-    }
-
-    public Long getPmax(LwM2mPath path, NotificationAttributeTree attributes) {
-        LwM2mAttributeSet set = attributes.get(path);
-        if (set == null)
-            return null;
-        else {
-            LwM2mAttribute<Long> pmax = set.get(LwM2mAttributes.MAXIMUM_PERIOD);
-            if (pmax == null)
-                return null;
-            else
-                return pmax.getValue();
-        }
-    }
-
-    public boolean hasCriteriaBasedOnValue(LwM2mPath path, NotificationAttributeTree attributes) {
-        LwM2mAttributeSet set = attributes.get(path);
-        return set != null && (set.contains(LwM2mAttributes.GREATER_THAN) || set.contains(LwM2mAttributes.LESSER_THAN)
-                || set.contains(LwM2mAttributes.STEP));
-    }
-
-    public boolean shouldTriggerNotificationBasedOnValueChange(LwM2mPath path, NotificationAttributeTree attributes,
-            LwM2mNode lastSentNode, LwM2mChildNode newNode) {
-
-        // Get Previous and New Values
-        Object lastSentValue;
-        Object newValue;
-        Type resourceType;
-        if (lastSentNode instanceof LwM2mSingleResource && newNode instanceof LwM2mSingleResource) {
-            lastSentValue = ((LwM2mSingleResource) lastSentNode).getValue();
-            newValue = ((LwM2mSingleResource) newNode).getValue();
-            resourceType = ((LwM2mSingleResource) newNode).getType();
-        } else if (lastSentNode instanceof LwM2mResourceInstance && newNode instanceof LwM2mResourceInstance) {
-            lastSentValue = ((LwM2mResourceInstance) lastSentNode).getValue();
-            newValue = ((LwM2mResourceInstance) newNode).getValue();
-            resourceType = ((LwM2mResourceInstance) newNode).getType();
-        } else {
-            throw new IllegalArgumentException(String.format(
-                    "Unexpected nodes (last send node  %s new value %s) for check about value changed, only LwM2mSingleResource or LwM2mResourceInstance are supported",
-                    lastSentNode.getClass().getSimpleName(), newNode.getClass().getSimpleName()));
-        }
-
-        // Check criteria
-        CriteriaBasedOnValueChecker checker = checkers.get(resourceType);
-        if (checker == null) {
-            throw new IllegalArgumentException(
-                    String.format("Unexpected resource type : %s is not supported", resourceType));
-        }
-        LwM2mAttributeSet set = attributes.get(path);
-        return checker.shouldTriggerNotificationBasedOnValueChange(set, lastSentValue, newValue);
-    }
-
+    /**
+     * @param attributes The attributes returned by
+     *        {@link #selectNotificationsAttributes(LwM2mPath, NotificationAttributeTree)}
+     * @param path The path of the LWM2M node to observe
+     * @param lastSentNode The last LwM2mNode sent in a notification for this observe relation.
+     * @param newNode The a new value for which we should decide if a new notification should be sent following criteria
+     *        based on value.
+     * @return <code>True</code> if a new notification should be sent.
+     */
+    boolean shouldTriggerNotificationBasedOnValueChange(NotificationAttributeTree attributes, LwM2mPath path,
+            LwM2mNode lastSentNode, LwM2mChildNode newNode);
 }
