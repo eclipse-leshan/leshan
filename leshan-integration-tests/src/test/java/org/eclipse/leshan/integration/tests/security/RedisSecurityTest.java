74ba7bde59cc59c9871c6e2c9ac04c2bb2c0e5eb
Simon Bernard <code@simonbernard.eu>
2024-04-05 18:10:20 +0200

    [integration tests] Move Security tests in dedicated package

--- leshan-integration-tests/src/test/java/org/eclipse/leshan/integration/tests/security/RedisSecurityTest.java
+++ leshan-integration-tests/src/test/java/org/eclipse/leshan/integration/tests/security/RedisSecurityTest.java
@@ -13,7 +13,7 @@
  * Contributors:
  *     Sierra Wireless - initial API and implementation
  *******************************************************************************/
-package org.eclipse.leshan.integration.tests;
+package org.eclipse.leshan.integration.tests.security;
 
 import org.eclipse.leshan.core.endpoint.Protocol;
 import org.eclipse.leshan.integration.tests.util.LeshanTestServerBuilder;
