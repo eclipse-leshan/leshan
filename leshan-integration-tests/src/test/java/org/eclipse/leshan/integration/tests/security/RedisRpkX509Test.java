405f4f2e7728bdc4feeb9bc174211e598b3ce590
Simon Bernard <code@simonbernard.eu>
2024-04-05 18:26:21 +0200

    [integration tests] Split SecurityTest in several class

---
+++ leshan-integration-tests/src/test/java/org/eclipse/leshan/integration/tests/security/RedisRpkX509Test.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Sierra Wireless and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ *
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v20.html
+ * and the Eclipse Distribution License is available at
+ *    http://www.eclipse.org/org/documents/edl-v10.html.
+ *
+ * Contributors:
+ *     Sierra Wireless - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.leshan.integration.tests.security;
+
+import org.eclipse.leshan.core.endpoint.Protocol;
+import org.eclipse.leshan.integration.tests.util.LeshanTestServerBuilder;
+import org.eclipse.leshan.integration.tests.util.RedisTestUtil;
+import org.eclipse.leshan.server.redis.RedisSecurityStore;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.util.Pool;
+
+public class RedisRpkX509Test extends RpkX509Test {
+
+    @Override
+    protected LeshanTestServerBuilder givenServerUsing(Protocol givenProtocol) {
+        LeshanTestServerBuilder builder = super.givenServerUsing(givenProtocol);
+
+        // Create redis store
+        Pool<Jedis> jedis = RedisTestUtil.createJedisPool();
+        // TODO use custom key when https://github.com/eclipse/leshan/issues/1249 will be available
+
+        // TODO should we also use RedisRegistrationStore ?
+        // builder.setRegistrationStore(new RedisRegistrationStore(jedis));
+        builder.setSecurityStore(new RedisSecurityStore(jedis));
+        return builder;
+    }
+}
