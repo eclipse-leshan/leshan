688dc503b4e5e5bb8b4bdf3fe7388d68e76ef6f9
Simon Bernard <code@simonbernard.eu>
2024-04-05 18:09:38 +0200

    [integration tests] Move oscore tests in a dedicated package

--- leshan-integration-tests/src/test/java/org/eclipse/leshan/integration/tests/oscore/OscoreBootstrapTest.java
+++ leshan-integration-tests/src/test/java/org/eclipse/leshan/integration/tests/oscore/OscoreBootstrapTest.java
@@ -13,7 +13,7 @@
  * Contributors:
  *     Sierra Wireless - initial API and implementation
  *******************************************************************************/
-package org.eclipse.leshan.integration.tests;
+package org.eclipse.leshan.integration.tests.oscore;
 
 import static org.eclipse.leshan.integration.tests.util.BootstrapConfigTestBuilder.givenBootstrapConfig;
 import static org.eclipse.leshan.integration.tests.util.Credentials.OSCORE_AEAD_ALGORITHM;
