2068280405867f771086cf2acbbf6422d9357e54
Simon Bernard <code@simonbernard.eu>
2025-10-31 11:27:13 +0100

    enable some oscore test in RedisSecurityStoreTest

--- leshan-integration-tests/src/test/java/org/eclipse/leshan/integration/tests/security/RedisSecurityStoreTest.java
+++ leshan-integration-tests/src/test/java/org/eclipse/leshan/integration/tests/security/RedisSecurityStoreTest.java
@@ -17,28 +17,11 @@ package org.eclipse.leshan.integration.tests.security;
 
 import org.eclipse.leshan.core.endpoint.Protocol;
 import org.eclipse.leshan.integration.tests.util.LeshanTestServerBuilder;
-import org.eclipse.leshan.servers.security.NonUniqueSecurityInfoException;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 
 public class RedisSecurityStoreTest extends SecurityStoreTest {
 
     @Override
     protected LeshanTestServerBuilder givenServerUsing(Protocol givenProtocol) {
         return super.givenServerUsing(givenProtocol).withRedisRegistrationStore().withRedisSecurityStore();
-    }
-
-    @Override
-    @Test
-    @Disabled("OSCORE not supported yet")
-    void change_oscore_rid_cleanup() throws NonUniqueSecurityInfoException {
-        // "OSCORE not supported yet"
-    }
-
-    @Override
-    @Test
-    @Disabled("OSCORE not supported yet")
-    void nonunique_oscore_rid() throws NonUniqueSecurityInfoException {
-        // "OSCORE not supported yet"
     }
 }
